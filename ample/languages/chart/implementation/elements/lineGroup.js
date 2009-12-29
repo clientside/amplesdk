9af872914a4f7ad94b2792958d92d16e76849b1e
ilinsky <castonet@yahoo.co.uk>
2009-12-29 15:30:10 +0100

    Line item figures implemented

--- ample/languages/chart/implementation/elements/lineGroup.js
+++ ample/languages/chart/implementation/elements/lineGroup.js
@@ -11,9 +11,55 @@ var cChartElement_lineGroup	= function(){};
 cChartElement_lineGroup.prototype	= new cChartElement;
 cChartElement_lineGroup.prototype.$hoverable	= true;
 
+cChartElement_lineGroup.getMarkerPath	= function(nX, nY, nType) {
+	var nSize	= 3,
+		nSize2	= nSize * 2;
+	switch (nType) {
+		// Square
+		case 1:
+			return	"M" +(nX - nSize)+ "," +(nY - nSize)+
+					"h" + nSize2 +
+					"v" + nSize2 +
+					"h-" + nSize2 +
+					"v-" + nSize2 +
+					"z";
+		// Triangle
+		case 2:
+			var nOffset	= nSize2 / Math.sqrt(3);
+			return	"M" +(nX - nOffset)+ "," +(nY - nSize)+
+					"h" + nOffset * 2 +
+					"l-" + nOffset + "," + nSize2 +
+					"l-" + nOffset + ",-" + nSize2 +
+					"z";
+		// Square 45%
+		case 3:
+			return "M" + nX + "," +(nY - nSize)+
+					"l" + nSize + "," + nSize +
+					"l-" + nSize + "," + nSize +
+					"l-" + nSize + ",-" + nSize +
+					"l" + nSize + ",-" + nSize +
+					"z";
+		// Triangle 60%
+		case 4:
+			var nOffset	= nSize2 / Math.sqrt(3);
+			return	"M" + nX + "," +(nY - nSize)+
+					"l" + nOffset + "," + nSize2 +
+					"h-" + nOffset * 2 +
+					"l" + nOffset + ",-" + nSize2 +
+					"z";
+		// Circle
+		default:
+			return	"M" + (nX - nSize) + "," + nY +
+					"a" + nSize + "," + nSize + " 0 0,0 " + nSize2 + ",0 " +
+					"a" + nSize + "," + nSize + " 0 0,0 -" + nSize2 + ",0 " +
+					"z";
+	}
+};
+
 cChartElement_lineGroup.prototype.refresh	= function() {
 	var xAxisRange	= this.parentNode.getAttribute("xAxisRange").split(';'),
 		yAxisRange	= this.parentNode.getAttribute("yAxisRange").split(';'),
+		nGroup	= this.parentNode.childNodes.$indexOf(this),
 		aValues	= [],
 		nIndex, nLength;
 
@@ -38,8 +84,8 @@ cChartElement_lineGroup.prototype.refresh	= function() {
 		oElementDOM	= this.childNodes[nIndex].$getContainer();
 		nX	= 50 + aValues[nIndex][0] * 500 / (xAxisRange[1] - xAxisRange[0]);
 		nY	= 250- aValues[nIndex][1] * 200 / (yAxisRange[1] - yAxisRange[0]);
-		oElementDOM.setAttribute("cx", nX);
-		oElementDOM.setAttribute("cy", nY);
+		oElementDOM.setAttribute("d", cChartElement_lineGroup.getMarkerPath(nX, nY, nGroup));
+		//
 		d.push(nX + "," + nY + " ");
 		if (!nIndex) {
 			nXFrom	= nX;
--- ample/languages/chart/implementation/elements/lineItem.js
+++ ample/languages/chart/implementation/elements/lineItem.js
@@ -11,7 +11,7 @@ var cChartElement_lineItem	= function(){};
 cChartElement_lineItem.prototype	= new cChartElement;
 
 cChartElement_lineItem.prototype.$getTagOpen	= function() {
-	return '<svg:circle r="3" class="c-lineItem' +(this.hasAttribute("class") ? ' ' + this.getAttribute("class") : '')+ '" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" />';
+	return '<svg:path class="c-lineItem' +(this.hasAttribute("class") ? ' ' + this.getAttribute("class") : '')+ '" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" />';
 };
 
 // Register Element with language
