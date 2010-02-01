a47fe73417387b56abeb3c9a1187311ffac19070
ilinsky <castonet@yahoo.co.uk>
2010-02-01 17:23:08 +0100

    Implemented line shadow content for IE

--- ample/languages/chart/implementation/elements/line.js
+++ ample/languages/chart/implementation/elements/line.js
@@ -12,7 +12,10 @@ cChartElement_line.prototype	= new cChartElement;
 
 cChartElement_line.handlers	= {
 	'DOMNodeInsertedIntoDocument':	function() {
-		this.refresh();
+		var that	= this;
+		setTimeout(function() {
+			that.refresh();
+		});
 	}
 };
 
@@ -62,7 +65,7 @@ cChartElement_line.prototype.refresh	= function() {
 		d.push("M" + (50 + x * 50) + ",50 v200 z ");
 	for (var y = 1; y < 4; y++)
 		d.push("M50," + (250 - y * 50) + "h400 z ");
-	this.$getContainer("grid").setAttribute("d", d.join(''));
+	cChartElement.setPath(this.$getContainer("grid"), d.join(''));
 
 	var aYSumUp	=[],
 		nYFromPrev = 250, nYToPrev = 250, dPrev = [];
@@ -98,7 +101,7 @@ cChartElement_line.prototype.refresh	= function() {
 
 			//
 			if (!bArea)
-				oGroup.childNodes[nItem].$getContainer("value").setAttribute("d", cChartElement_line.getMarkerPath(nX, nY, nGroup));
+				cChartElement.setPath(oGroup.childNodes[nItem].$getContainer("value"), cChartElement_line.getMarkerPath(nX, nY, nGroup));
 			//
 			d.push(nX + "," + nY + " ");
 
@@ -114,18 +117,19 @@ cChartElement_line.prototype.refresh	= function() {
 		}
 
 		// Draw line
-		oGroup.$getContainer("line").setAttribute("d", "M" + nXFrom + "," + nYFrom + " L" + d.join(''));
-		oGroup.$getContainer("shadow").setAttribute("d", "M" + nXFrom + "," + nYFrom + " L" + d.join(''));
+		cChartElement.setPath(oGroup.$getContainer("line"), "M" + nXFrom + "," + nYFrom + " L" + d.join(''));
+		cChartElement.setPath(oGroup.$getContainer("shadow"), "M" + nXFrom + "," + nYFrom + " L" + d.join(''));
 		if (bArea)
-			oGroup.$getContainer("area").setAttribute("d", "M" + nXFrom + "," + nYFromPrev + " L" + d.join('') + (this.hasAttribute("type") ? dPrev.reverse().join('') : '') + " L" + nXTo + "," + nYToPrev + "z");
+			cChartElement.setPath(oGroup.$getContainer("area"), "M" + nXFrom + "," + nYFromPrev + " L" + d.join('') + (this.hasAttribute("type") ? dPrev.reverse().join('') : '') + " L" + nXTo + "," + nYToPrev + "z");
 
 		// Draw legend
 		var nXPath	= 480,
 			nYPath	=(50 + (nGroups - nGroup) * 20),
 			sLine	= this.getAttribute("area") == "true" ? "h20" : " h7 z M" + (nXPath + 10) + "," + nYPath + " h-7";
-		oGroup.$getContainer("path").setAttribute("d", "M" + (nXPath - 10) + "," + nYPath + sLine + "z" + (this.getAttribute("area") == "true" ? '' : cChartElement_line.getMarkerPath(nXPath, nYPath, nGroup)));
-		oGroup.$getContainer("label").setAttribute("x", nXPath + 20);
-		oGroup.$getContainer("label").setAttribute("y", nYPath + 5);
+		cChartElement.setPath(oGroup.$getContainer("path"), "M" + (nXPath - 10) + "," + nYPath + sLine + "z" + (this.getAttribute("area") == "true" ? '' : cChartElement_line.getMarkerPath(nXPath, nYPath, nGroup)));
+		cChartElement.setTextPosition(oGroup.$getContainer("label"),
+										nXPath + 20,
+										nYPath + 5);
 
 		//
 		if (this.hasAttribute("type")) {
@@ -136,30 +140,65 @@ cChartElement_line.prototype.refresh	= function() {
 	}
 };
 
-cChartElement_line.prototype.$getTagOpen	= function() {
-	return '<div class="c-line' +(this.hasAttribute("class") ? ' ' + this.getAttribute("class") : '')+ '" style="' + this.getAttribute("style") + '">\
-				<svg:svg class="c-line--canvas" viewBox="0 0 600 300" width="600px" height="300px" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\
-					<svg:text class="c-line--title" y="30" x="300">' + this.getAttribute("title")+ '</svg:text>\
-					<svg:rect x="460" y="50" width="120" height="120" rx="10" class="c-legend"/>\
-					<svg:path class="c-grid c-line--grid"/>\
-					<svg:g class="c-xAxis">\
-						<svg:path class="c-line--xAxis" d="m50,250 h400,0" style="fill:none"/>\
-						<svg:path id="x' + this.uniqueID + '" d="m300,280 h400,0" style="fill:none;stroke:none"/>\
-						<svg:text class="c-xAxis--label c-line--xAxisLabel"><svg:textPath xlink:href="#x' + this.uniqueID + '">' + this.getAttribute("xAxisLabel")+ '</svg:textPath></svg:text>\
-					</svg:g>\
-					<svg:g class="c-yAxis">\
-						<svg:path class="c-line--yAxis" d="m50,250 v0,-200" style="fill:none"/>\
-						<svg:path id="y' + this.uniqueID + '" d="m30,200 v0,-200" style="fill:none;stroke:none"/>\
-						<svg:text class="c-yAxis--label c-line--yAxisLabel"><svg:textPath xlink:href="#y' + this.uniqueID + '">' + this.getAttribute("yAxisLabel")+ '</svg:textPath></svg:text>\
-					</svg:g>\
-					<svg:g class="c-line--gateway">';
-};
+if (!cChartElement.useVML) {
+	cChartElement_line.prototype.$getTagOpen	= function() {
+		return '<div class="c-line' +(this.hasAttribute("class") ? ' ' + this.getAttribute("class") : '')+ '" style="' + this.getAttribute("style") + '">\
+					<svg:svg class="c-line--canvas" viewBox="0 0 600 300" width="600px" height="300px" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\
+						<svg:text class="c-line--title" y="30" x="300">' + this.getAttribute("title")+ '</svg:text>\
+						<svg:rect x="460" y="50" width="120" height="120" rx="10" class="c-legend"/>\
+						<svg:path class="c-grid c-line--grid"/>\
+						<svg:g class="c-xAxis">\
+							<svg:path class="c-line--xAxis" d="m50,250 h400,0" style="fill:none"/>\
+							<svg:path id="x' + this.uniqueID + '" d="m300,280 h400,0" style="fill:none;stroke:none"/>\
+							<svg:text class="c-xAxis--label c-line--xAxisLabel"><svg:textPath xlink:href="#x' + this.uniqueID + '">' + this.getAttribute("xAxisLabel")+ '</svg:textPath></svg:text>\
+						</svg:g>\
+						<svg:g class="c-yAxis">\
+							<svg:path class="c-line--yAxis" d="m50,250 v0,-200" style="fill:none"/>\
+							<svg:path id="y' + this.uniqueID + '" d="m30,200 v0,-200" style="fill:none;stroke:none"/>\
+							<svg:text class="c-yAxis--label c-line--yAxisLabel"><svg:textPath xlink:href="#y' + this.uniqueID + '">' + this.getAttribute("yAxisLabel")+ '</svg:textPath></svg:text>\
+						</svg:g>\
+						<svg:g class="c-line--gateway">';
+	};
 
-cChartElement_line.prototype.$getTagClose	= function() {
-	return '		</svg:g>\
-				</svg:svg>\
-			</div>';
-};
+	cChartElement_line.prototype.$getTagClose	= function() {
+		return '		</svg:g>\
+					</svg:svg>\
+				</div>';
+	};
+}
+else {
+	cChartElement_line.prototype.$getTagOpen	= function() {
+		return '<div class="c-line' +(this.hasAttribute("class") ? ' ' + this.getAttribute("class") : '')+ '" style="' + this.getAttribute("style") + '">\
+					<chart2vml:group class="c-line--canvas" style="position:relative;display:inline-block;x-overflow:hidden;width:600px;height:300px" coordOrigin="0 0" coordSize="600 300">\
+						<chart2vml:shape path="m0,0 l600,0" fillcolor="blue" stroked="false" allowoverlap="true" style="position:absolute;width:100%;height:100%;top:30px;xleft:150px">\
+							<chart2vml:path textpathok="true" />\
+							<chart2vml:textpath on="true" class="c-line--title" string="' + this.getAttribute("title")+ '" style="v-text-align:center"/>\
+						</chart2vml:shape>\
+						<chart2vml:roundrect style="left:460px;top:50px;width:120px;height:120px" rx="10" class="c-legend" filled="true"/>\
+						<chart2vml:shape class="c-grid c-line--grid" style="position:absolute;width:100%;height:100%"/>\
+						<chart2vml:group class="c-xAxis" style="position:absolute;width:100%;height:100%">\
+							<chart2vml:shape class="c-line--xAxis" path="m50,250 r400,0 e" style="position:absolute;width:100%;height:100%"/>\
+							<chart2vml:shape path="m30,280 r400,0 e" fillcolor="blue" stroked="false" allowoverlap="true" style="position:absolute;width:100%;height:100%;top:30px;xleft:150px">\
+								<chart2vml:path textpathok="true" />\
+								<chart2vml:textpath on="true" class="c-xAxis--label c-line--xAxisLabel" string="' + this.getAttribute("xAxisLabel")+ '" style="v-text-align:center"/>\
+							</chart2vml:shape>\
+						</chart2vml:group>\
+						<chart2vml:group class="c-yAxis" style="position:absolute;width:100%;height:100%">\
+							<chart2vml:shape class="c-line--yAxis" path="m50,250 r0,-200 e" style="position:absolute;width:100%;height:100%"/>\
+							<chart2vml:shape path="m30,200 r0,-200 e" fillcolor="blue" stroked="false" allowoverlap="true" style="position:absolute;width:100%;height:100%;top:30px;xleft:150px">\
+								<chart2vml:path textpathok="true" />\
+								<chart2vml:textpath on="true" class="c-xAxis--label c-line--yAxisLabel" string="' + this.getAttribute("yAxisLabel")+ '" style="v-text-align:center"/>\
+							</chart2vml:shape>\
+						</chart2vml:group>\
+						<chart2vml:group class="c-line--gateway" style="position:absolute;width:100%;height:100%">';
+	};
+
+	cChartElement_line.prototype.$getTagClose	= function() {
+		return '		</chart2vml:group>\
+					</chart2vml:group>\
+				</div>';
+	};
+}
 
 // Static members
 cChartElement_line.getMarkerPath	= function(nX, nY, nType) {
