7d08a1d1e3390922709ab2abf08ff081e08108e7
ilinsky <castonet@yahoo.co.uk>
2010-02-01 17:11:41 +0100

    Corrected shadow content for IE

--- ample/languages/chart/implementation/elements/group.js
+++ ample/languages/chart/implementation/elements/group.js
@@ -47,13 +47,12 @@ else {
 		return '<chart2vml:group class="c-group c-group_nth-child-' + this.parentNode.childNodes.$indexOf(this) +(this.hasAttribute("class") ? ' ' + this.getAttribute("class") : '')+ '" \
 					style="position:absolute;width:100%;height:100%;' + this.getAttribute("style") + '">\
 					<chart2vml:shape class="c-group--path" style="position:absolute;width:100%;height:100%" />\
-					<chart2vml:shape class="c-group--shadow" filled="none" style="position:absolute;width:100%;height:100%" transform="translate(2, 2)"/>\
+					<chart2vml:shape class="c-group--shadow" filled="false" style="position:absolute;width:100%;height:100%" transform="translate(2, 2)"/>\
 					<chart2vml:shape class="c-group--line" filled="false" style="position:absolute;width:100%;height:100%"/>\
 					<chart2vml:shape class="c-group--area" stroked="false" style="position:absolute;width:100%;height:100%"/>\
-					<chart2vml:shape path="m0,0 l 100,0 xe" allowoverlap="true" style="position:absolute;width:100%;height:100%;">\
-						<chart2vml:fill on="true" color="red" />\
+					<chart2vml:shape path="m0,0 l100,0" fillcolor="red" stroked="false" allowoverlap="true" style="position:absolute;width:100%;height:100%;top:0;left:0;margin-left:0;margin-top:0">\
 						<chart2vml:path textpathok="true" />\
-						<chart2vml:textpath on="true" class="c-group--label" string="' + this.getAttribute("title")+ '" />\
+						<chart2vml:textpath on="true" class="c-group--label" string="' + this.getAttribute("label")+ '" style="v-text-align:left" />\
 					</chart2vml:shape>\
 					<chart2vml:group class="c-group--gateway" style="position:absolute;width:100%;height:100%">';
 	};
