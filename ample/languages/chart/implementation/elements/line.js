c564a80aac28603d9a777c46fc70600a7ff1749d
ilinsky <castonet@yahoo.co.uk>
2010-02-10 11:35:08 +0100

    Fixed several class names

--- ample/languages/chart/implementation/elements/line.js
+++ ample/languages/chart/implementation/elements/line.js
@@ -204,8 +204,8 @@ else {
 								<chart2vml:path textpathok="true" />\
 								<chart2vml:textpath on="true" string="' + this.getAttribute("xAxisLabel")+ '" style="v-text-align:center"/>\
 							</chart2vml:shape>\
-							<chart2vml:shape class="c-xAxis--marks c-bar--xAxisMarks" style="position:absolute;width:100%;height:100%"/>\
-							<chart2vml:group class="c-xAxis--scale c-bar--xAxisItems" style="top:3px"/>\
+							<chart2vml:shape class="c-xAxis--marks c-line--xAxisMarks" style="position:absolute;width:100%;height:100%"/>\
+							<chart2vml:group class="c-xAxis--scale c-line--xAxisItems" style="top:3px"/>\
 						</chart2vml:group>\
 						<chart2vml:group class="c-yAxis" style="position:absolute;width:100%;height:100%">\
 							<chart2vml:shape class="c-line--yAxis" path="m50,250 r0,-200 e" style="position:absolute;width:100%;height:100%"/>\
@@ -213,8 +213,8 @@ else {
 								<chart2vml:path textpathok="true" />\
 								<chart2vml:textpath on="true" string="' + this.getAttribute("yAxisLabel")+ '" style="v-text-align:center"/>\
 							</chart2vml:shape>\
-							<chart2vml:shape class="c-yAxis--marks c-bar--yAxisMarks" style="position:absolute;width:100%;height:100%"/>\
-							<chart2vml:group class="c-yAxis--scale c-bar--yAxisItems" style="top:3px"/>\
+							<chart2vml:shape class="c-yAxis--marks c-line--yAxisMarks" style="position:absolute;width:100%;height:100%"/>\
+							<chart2vml:group class="c-yAxis--scale c-line--yAxisItems" style="top:3px"/>\
 						</chart2vml:group>\
 						<chart2vml:group class="c-line--gateway" style="position:absolute;width:100%;height:100%">';
 	};
