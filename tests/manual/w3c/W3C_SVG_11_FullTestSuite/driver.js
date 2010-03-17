c2369d11ca8297d811cff3a021aa7ea50d078ec2
ilinsky <castonet@yahoo.co.uk>
2010-03-17 16:51:50 +0100

    Added W3C SVG 1.1 Full Test Suite

---
+++ tests/manual/w3c/W3C_SVG_11_FullTestSuite/driver.js
@@ -0,0 +1,42 @@
+// Load ample files
+var oRequest	= new XMLHttpRequest;
+// #include runtime
+oRequest.open("GET", "../../../../../ample/runtime.js", false);
+oRequest.send(null);
+eval(oRequest.responseText);
+// #include SVG implementationoRequest.open("GET", "../../../../../ample/languages/svg/svg.js", false);
+oRequest.send(null);
+eval(oRequest.responseText);
+
+// Load and and replace embed tag
+ample.addEventListener("load", function() {
+	var oElementOld	= document.getElementsByTagName("embed")[0],
+		sLocation	= oElementOld.getAttribute("src"),
+		sWidth		= oElementOld.getAttribute("width"),
+		sHeight		= oElementOld.getAttribute("height"),
+		oElementNew	= document.createElement("div"),
+		oElement	= ample.documentElement.appendChild(ample.createElement("fake"));
+	// Replace embed element with div
+	oElementOld.parentNode.replaceChild(oElementNew, oElementOld);
+	oElementNew.parentNode.setAttribute("align", "left");
+	oElementNew.style.width		= sWidth + "px";
+	oElementNew.style.height	= sHeight + "px";
+	// Bind Ample SDK fake element with browser DOM
+	oElementNew.setAttribute("id", "example");
+	oElement.setAttribute("id", "example");
+	// Load example
+	var oRequest = new XMLHttpRequest;
+	oRequest.open("GET", sLocation, true);
+	oRequest.onreadystatechange	= function() {
+		if (oRequest.readyState == 4) {
+			// Reparse
+			var oDocument	= new DOMParser().parseFromString(oRequest.responseText, "text/xml");
+			// Ample SDK doesn't support percent units yet!
+			oDocument.documentElement.setAttribute("width", sWidth);
+			oDocument.documentElement.setAttribute("height", sHeight);
+			// Append SVG fragment to Ample SDK DOM
+			oElement.appendChild(ample.importNode(oDocument.documentElement, true));
+		}
+	}
+	oRequest.send(null);
+}, false);
