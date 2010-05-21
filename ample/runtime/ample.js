clientside/amplesdk

commit 113e5ee247517e7d0515c583335669c95e4a8087

Enabled support for HTML5 fragments in core, HTML5 to be implemented

ample/runtime/ample.js
@@ -150,8 +150,8 @@ function fAML_import(oElementDOM, oNode, bCollapse) {
 					fAML_import(oElementDOM, oNode);
 			}
 			else {
-				// Create element
-				var oElement	= fAMLDocument_createElementNS(oAML_document, oElementDOM.namespaceURI, oElementDOM.nodeName),
+				// Create element (note: in IE, namespaceURI is empty string if not specified, hence "oElementDOM.namespaceURI || null")
+				var oElement	= fAMLDocument_createElementNS(oAML_document, oElementDOM.namespaceURI || null, oElementDOM.nodeName),
 					sNameSpaceURI	= oElement.namespaceURI,
 					sLocalName	= oElement.localName,
 					oAttributes	= oElement.attributes,
@@ -516,10 +516,6 @@ function fAML_processScripts() {
                 		oAttributes[sAttribute]	= fAML_encodeEntities(sAttribute == "style" ? oElementDOM[sAttribute].cssText : oAttribute.nodeValue);
 			}
 
-			// Add default namespace if missing
-			if (!oAttributes["xmlns"])
-				oAttributes["xmlns"]	= "http://www.w3.org/1999/xhtml";
-
 			if (oElementDOM.getAttribute("src")) {
 				var oRequest	= new cXMLHttpRequest;
 				oRequest.open("GET", oElementDOM.src, false);
@@ -575,6 +571,11 @@ function fAML_processScripts() {
 					// duplicate id problem
 		    		if (!bReferenced && !oAttributes['id'])
 		    			oAttributes['id']	= oElement.uniqueID;
+
+					// Add default namespace if missing (for rendering only)
+					if (!oAttributes["xmlns"])
+						oAttributes["xmlns"]	= "http://www.w3.org/1999/xhtml";
+
 		    		oElementNew	= oUADocument.importNode(new cDOMParser().parseFromString('<!' + "DOCTYPE" + ' ' + "div" + ' ' + '[' + sAML_entities + ']>' +
 //->Debug
 																		'\n' +

ample/runtime/classes/core/AMLConfiguration.js
@@ -48,7 +48,7 @@ cAMLConfiguration.prototype.canSetParameter	= function(sName, vValue) {
 cAMLConfiguration.prototype.setNamespace	= function(sNameSpaceURI, oNamespace) {
 	// Validate arguments
 	fAML_validate(arguments, [
-		["namespaceURI",	cString],
+		["namespaceURI",	cString,	true, true],
 		["namespace",		cAMLNamespace]
 	], "setNamespace");
 
@@ -59,7 +59,7 @@ cAMLConfiguration.prototype.setNamespace	= function(sNameSpaceURI, oNamespace) {
 cAMLConfiguration.prototype.getNamespace	= function(sNameSpaceURI) {
 	// Validate arguments
 	fAML_validate(arguments, [
-		["namespaceURI",	cString]
+		["namespaceURI",	cString,	true, true]
 	], "getNamespace");
 
 	return oAML_namespaces[sNameSpaceURI] || null;

commit 0b45c5f9137a4c5fe3e2b97f7e35172772e59217

Fixing IE rendering issue introduced injected in commit 113e5ee247517e7d0515

ample/runtime/ample.js
@@ -530,6 +530,8 @@ function fAML_processScripts() {
 			else {
 				if (!oAttributes["xmlns" + ':' + "aml"])
 					oAttributes["xmlns" + ':' + "aml"]	= "http://www.amplesdk.com/ns/aml";
+				if (!oAttributes["xmlns"] && oAML_document.namespaceURI)
+					oAttributes["xmlns"]	= oAML_document.namespaceURI;
 
 				// Create fragment
 			    oDocument   = new cDOMParser().parseFromString(//		"<?" + "xml" + ' ' + 'version="1.0"' + "?>" +