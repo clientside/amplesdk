86d5f38b8a7013447e894fdfa9c83966e0814d56
ilinsky <sergey@ilinsky.com>
2010-06-11 12:35:01 +0200

    Brushed up some

--- ample/runtime/classes/core/AMLNode.js
+++ ample/runtime/classes/core/AMLNode.js
@@ -727,79 +727,89 @@ cAMLNode.prototype.$getTag	= function()
 	return '';
 };
 
-cAMLNode.prototype.toXML	= function() {
-
-	var aHtml	= [];
+function fAMLNode_toXML(oNode)
+{
+	var aHtml	= [],
+		nIndex	= 0;
 //->Source
-	var nDepth	= 1;
+	var nDepth	= arguments.length > 1 ? arguments[1] : 1;
+	aHtml.push(new cArray(nDepth).join('\t'));
 //<-Source
-	(function(oNode) {
-		var nIndex = 0, sName, oAttributes;
+	switch (oNode.nodeType) {
+		case cAMLNode.ELEMENT_NODE:
+			var sName, oAttributes;
+			aHtml.push('<' + oNode.nodeName);
+			oAttributes	= oNode.attributes;
+			for (sName in oAttributes)
+				if (oAttributes.hasOwnProperty(sName))
+					aHtml.push(' ' + sName + '=' + '"' + fAML_encodeEntities(oAttributes[sName]) + '"');
+//			aHtml.push(' ' + '_' + '=' + '"' + oNode.uniqueID + '"');
+			if (oNode.hasChildNodes()) {
+				aHtml.push('>');
 //->Source
-		aHtml.push(new cArray(nDepth).join('\t'));
+				aHtml.push('\n');
+				nDepth++;
 //<-Source
-		switch (oNode.nodeType) {
-			case cAMLNode.ELEMENT_NODE:
-				aHtml.push('<' + oNode.nodeName);
-				oAttributes	= oNode.attributes;
-				for (sName in oAttributes)
-					if (oAttributes.hasOwnProperty(sName))
-						aHtml.push(' ' + sName + '=' + '"' + fAML_encodeEntities(oAttributes[sName]) + '"');
-	//			aHtml.push(' ' + '_' + '=' + '"' + oNode.uniqueID + '"');
-				if (oNode.hasChildNodes()) {
-					aHtml.push('>');
-//->Source
-					aHtml.push('\n');
-					nDepth++;
-//<-Source
-					while (nIndex < oNode.childNodes.length)
-						arguments.callee(oNode.childNodes[nIndex++]);
-//->Source
-					nDepth--;
-					aHtml.push('\n');
-					aHtml.push(new cArray(nDepth).join('\t'));
-//<-Source
-					aHtml.push('</' + oNode.nodeName + '>');
-				}
-				else
-					aHtml.push('/>');
-				break;
-
-			case cAMLNode.TEXT_NODE:
-				aHtml.push(oNode.nodeValue);
-				break;
-
-			case cAMLNode.CDATA_SECTION_NODE:
-				aHtml.push('<![CDATA[' + oNode.nodeValue + ']]>');
-				break;
-
-			case cAMLNode.PROCESSING_INSTRUCTION_NODE:
-				aHtml.push('<?' + oNode.nodeName + ' ' + oNode.nodeValue + '?>');
-				break;
-
-			case cAMLNode.COMMENT_NODE:
-				aHtml.push('<!--' + oNode.nodeValue + '-->');
-				break;
-
-			case cAMLNode.DOCUMENT_FRAGMENT_NODE:
-			case cAMLNode.DOCUMENT_NODE:
 				while (nIndex < oNode.childNodes.length)
-					arguments.callee(oNode.childNodes[nIndex++]);
-				break;
-/*
-			case cAMLNode.NOTATION_NODE:
-			case cAMLNode.DOCUMENT_TYPE_NODE:
-			case cAMLNode.ENTITY_REFERENCE_NODE:
-			case cAMLNode.ENTITY_NODE:
-			case cAMLNode.ATTRIBUTE_NODE:
-*/
-			default:
-				throw new cAMLException(cAMLException.NOT_SUPPORTED_ERR);
-		}
+					aHtml.push(fAMLNode_toXML(oNode.childNodes[nIndex++]
 //->Source
-		if (oNode.nextSibling)
-			aHtml.push('\n');
+						, nDepth
 //<-Source
-	})(this);
+					));
+//->Source
+				nDepth--;
+				aHtml.push('\n');
+				aHtml.push(new cArray(nDepth).join('\t'));
+//<-Source
+				aHtml.push('</' + oNode.nodeName + '>');
+			}
+			else
+				aHtml.push('/>');
+			break;
+
+		case cAMLNode.TEXT_NODE:
+			aHtml.push(oNode.nodeValue);
+			break;
+
+		case cAMLNode.CDATA_SECTION_NODE:
+			aHtml.push('<![CDATA[' + oNode.nodeValue + ']]>');
+			break;
+
+		case cAMLNode.PROCESSING_INSTRUCTION_NODE:
+			aHtml.push('<?' + oNode.nodeName + ' ' + oNode.nodeValue + '?>');
+			break;
+
+		case cAMLNode.COMMENT_NODE:
+			aHtml.push('<!--' + oNode.nodeValue + '-->');
+			break;
+
+		case cAMLNode.DOCUMENT_FRAGMENT_NODE:
+		case cAMLNode.DOCUMENT_NODE:
+			while (nIndex < oNode.childNodes.length)
+				aHtml.push(fAMLNode_toXML(oNode.childNodes[nIndex++]
+//->Source
+					, nDepth
+//<-Source
+				));
+			break;
+/*
+		case cAMLNode.NOTATION_NODE:
+		case cAMLNode.DOCUMENT_TYPE_NODE:
+		case cAMLNode.ENTITY_REFERENCE_NODE:
+		case cAMLNode.ENTITY_NODE:
+		case cAMLNode.ATTRIBUTE_NODE:
+*/
+		default:
+			throw new cAMLException(cAMLException.NOT_SUPPORTED_ERR);
+	}
+//->Source
+	if (oNode.nextSibling)
+		aHtml.push('\n');
+//<-Source
 	return aHtml.join('');
+};
+
+cAMLNode.prototype.toXML	= function()
+{
+	return fAMLNode_toXML(this);
 };
