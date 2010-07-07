153ab2958ce5857f7adfd2e290ffd9535294cb55
ilinsky <sergey@ilinsky.com>
2010-07-07 18:02:23 +0200

    Re-factored to use divs instead of tables

--- ample/languages/xul/implementation/elements/progressmeter.js
+++ ample/languages/xul/implementation/elements/progressmeter.js
@@ -22,8 +22,8 @@ cXULElement_progressmeter.attributes.value	= "100";
 cXULElement_progressmeter.prototype._onInterval  = function() {
     this._left  = this._left + 1 > 100 + 30 ? 0 : this._left + 1;
 
-    this.$getContainer("units").style.left  =(this._left > 30 ? this._left - 30 : 0)+ '%';
-    this.$getContainer("units").style.width =(this._left < 30 ? this._left : 100 + 30 - this._left < 30 ? 100 + 30 - this._left : 30)+ '%';
+    this.$getContainer("value").style.left  =(this._left > 30 ? this._left - 30 : 0)+ '%';
+    this.$getContainer("value").style.width =(this._left < 30 ? this._left : 100 + 30 - this._left < 30 ? 100 + 30 - this._left : 30)+ '%';
 };
 
 // Class handlers
@@ -33,13 +33,13 @@ cXULElement_progressmeter.handlers	= {
 			switch (oEvent.attrName) {
 				case "value":
 					if (this.attributes["mode"] != "undetermined")
-						this.$getContainer("units").style.width = oEvent.newValue + '%';
+						this.$getContainer("value").style.width = oEvent.newValue + '%';
 					break;
 
 				case "mode":
 					if (oEvent.newValue == "undetermined") {
 						if (!this._interval) {
-							var oElementDOM	= this.$getContainer("units");
+							var oElementDOM	= this.$getContainer("value");
 							oElementDOM.style.width = '0%';
 							oElementDOM.style.left  = '0%';
 
@@ -55,7 +55,7 @@ cXULElement_progressmeter.handlers	= {
 							clearInterval(this._interval);
 							this._interval  = null;
 						}
-						var oElementDOM	= this.$getContainer("units");
+						var oElementDOM	= this.$getContainer("value");
 						oElementDOM.style.width = this.attributes["value"] + '%';
 						oElementDOM.style.left  = '0%';
 					}
@@ -82,22 +82,15 @@ cXULElement_progressmeter.handlers	= {
 
 // Element Render: open
 cXULElement_progressmeter.prototype.$getTagOpen	= function() {
-    var sHtml   = '<table cellpadding="0" cellspacing="0" border="0" width="' + (this.attributes["width"] || "100%") + '" class="xul-progressmeter"' +(this.attributes["hidden"] == "true" ? ' style="display:none;"' : "")+ '>';
-    sHtml  += '<tbody>';
-    sHtml  += '<tr>';
-    sHtml  += '<td class="xul-progressmeter-bar-left"><br /></td>';
-    sHtml  += '<td class="xul-progressmeter-bar-middle" valign="center">';
-    sHtml  += '<div style="position:relative;';
-    if (this.attributes["value"])
-        sHtml  += 'width:' + this.attributes["value"] + '%;';
-    sHtml  += '" class="xul-progressmeter--units"><br><img width="1" height="1"/></br></div>';
-    sHtml  += '</td>';
-    sHtml  += '<td class="xul-progressmeter-bar-right"><br /></td>';
-    sHtml  += '</tr>';
-    sHtml  += '</tbody>';
-    sHtml  += '</table>';
-
-    return sHtml;
+	return '<div class="xul-progressmeter" style="'+
+				'width:' + (this.attributes["width"] ? this.attributes["width"] : "100%") + ';' +
+				(this.attributes["hidden"] == "true" ? 'display:none;' : "") + '">\
+				<div class="xul-progressmeter--before" style="float:left"></div>\
+				<div class="xul-progressmeter--after" style="float:right"></div>\
+				<div class="xul-progressmeter--bar">\
+					<div class="xul-progressmeter--value" style="position:relative;font-size:1px;width:' + this.attributes["value"] + '%"></div>\
+				</div>\
+			</div>';
 };
 
 // Register Element with language
