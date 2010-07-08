0af1e58836107bc312c29975f4ae1bf517f4fade
ilinsky <sergey@ilinsky.com>
2010-07-08 13:15:19 +0200

    Re-organized language directories structure

--- ample/languages/chart/chart.php
+++ ample/languages/chart/chart.php
@@ -1,22 +1,22 @@
 <?
     $aFiles		= array();
-    $aFiles[]		= "implementation/chart.js";
-    $aFiles[]		= "implementation/classes/cChartElement.js";
+    $aFiles[]		= "chart.js";
+    $aFiles[]		= "classes/cChartElement.js";
 
     //
-    $aFiles[]		= "implementation/elements/bar.js";
-    $aFiles[]		= "implementation/elements/bubble.js";
-    $aFiles[]		= "implementation/elements/chart.js";
-    $aFiles[]		= "implementation/elements/funnel.js";
-    $aFiles[]		= "implementation/elements/line.js";
-    $aFiles[]		= "implementation/elements/map.js";
-    $aFiles[]		= "implementation/elements/doughnut.js";
-    $aFiles[]		= "implementation/elements/pie.js";
-    $aFiles[]		= "implementation/elements/radar.js";
-    $aFiles[]		= "implementation/elements/stream.js";
+    $aFiles[]		= "elements/bar.js";
+    $aFiles[]		= "elements/bubble.js";
+    $aFiles[]		= "elements/chart.js";
+    $aFiles[]		= "elements/funnel.js";
+    $aFiles[]		= "elements/line.js";
+    $aFiles[]		= "elements/map.js";
+    $aFiles[]		= "elements/doughnut.js";
+    $aFiles[]		= "elements/pie.js";
+    $aFiles[]		= "elements/radar.js";
+    $aFiles[]		= "elements/stream.js";
     //
-    $aFiles[]		= "implementation/elements/item.js";
-    $aFiles[]		= "implementation/elements/group.js";
+    $aFiles[]		= "elements/item.js";
+    $aFiles[]		= "elements/group.js";
 
     $sOutput	= "";
     for ($nIndex = 0; $nIndex < count($aFiles); $nIndex++)
