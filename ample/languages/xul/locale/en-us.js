09f6c168fb7805641825d54afdd4b5f603b9cbf4
ilinsky <sergey@ilinsky.com>
2010-07-09 18:28:52 +0200

    Added copyright header

--- ample/languages/xul/locale/en-us.js
+++ ample/languages/xul/locale/en-us.js
@@ -1,3 +1,12 @@
+/*
+ * Ample SDK - JavaScript GUI Framework
+ *
+ * Copyright (c) 2009 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ * See: http://www.amplesdk.com/about/licensing/
+ *
+ */
+
 oXULLocaleManager.register("en-US", {
 	dictionary: {
 		// datepicker-pane
--- ample/languages/xul/managers/oXULLocaleManager.js
+++ ample/languages/xul/managers/oXULLocaleManager.js
@@ -1,3 +1,12 @@
+/*
+ * Ample SDK - JavaScript GUI Framework
+ *
+ * Copyright (c) 2009 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ * See: http://www.amplesdk.com/about/licensing/
+ *
+ */
+
 var oXULLocaleManager	=(function(){
 	// Private variables
 	var oLocales	= {};
