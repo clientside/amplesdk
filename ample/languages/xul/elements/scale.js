1580adb644d9e09405c62b9ec2b38c8128619bd0
ilinsky <sergey@ilinsky.com>
2010-07-13 15:51:05 +0200

    scale component re-implemented, now with support for multiple handles and orientation

--- ample/languages/xul/elements/scale.js
+++ ample/languages/xul/elements/scale.js
@@ -7,185 +7,129 @@
  *
  */
 
-var cXULElement_scale	= function(){};
+var cXULElement_scale = function(){};
+
 cXULElement_scale.prototype	= new cXULInputElement;
 
-// Default Attributes
 cXULElement_scale.attributes	= {
-	"min":		"0",
-	"max":		"9",
-	"step":		"1",
-	"value":	"0"
+	"min":	"0",
+	"max":	"100",
+	"value":"0"
 };
 
-// Public Properties
-cXULElement_scale.prototype.form	= null;
+// Static Properties
+cXULElement_scale.captured	= false;
+cXULElement_scale.button	=-1;
+cXULElement_scale.clientRect= null;
+cXULElement_scale.prevValue	= "";
 
-//Public Methods
-cXULElement_scale.prototype.$getValue	= function() {
-	return this.$getContainer("input").value;
-};
-
-cXULElement_scale.prototype.$setValue	= function(sValue) {
-    if (sValue * 1.0 < this.attributes["min"] * 1.0 || isNaN(sValue))
-        sValue  = this.attributes["min"] * 1.0;
-    else
-    if (sValue * 1.0 > this.attributes["max"] * 1.0)
-        sValue  = this.attributes["max"] * 1.0;
-
-	var oPosition	= this.getBoundingClientRect();
-    this.$getContainer("button").style.left   = Math.round((oPosition.right - oPosition.left - 17) * (sValue - this.attributes["min"] * 1.0) / (this.attributes["max"] * 1.0 - this.attributes["min"] * 1.0)) + "px";
-
-    // Save value
-    this.$getContainer("input").value	= sValue;
-};
-
-// Events Handlers
-cXULElement_scale.prototype._onButtonMouseDown  = function(oEvent) {
-    if (!this.$isAccessible())
-        return false;
-
-    this._mouseX    = oEvent.clientX;
-    this._mouseY    = oEvent.clientY;
-
-    this._buttonX   = parseInt(this.$getContainer("button").style.left);
-    this._buttonY   = parseInt(this.$getContainer("button").style.top);
-
-	cXULElement_scale._element	= this;
-	this.ownerDocument.addEventListener("mousemove",	cXULElement_scale._onDocumentMouseMove,	false);
-	this.ownerDocument.addEventListener("mouseup",		cXULElement_scale._onDocumentMouseUp,		false);
-
-	this.$setPseudoClass("active", true, "button");
-
-	//
-	return false;
-};
-
-// Static Methods
-cXULElement_scale._onDocumentMouseUp    = function(oEvent) {
-	var oElement	= cXULElement_scale._element;
-
-	// detach element
-	delete cXULElement_scale._element;
-	oElement.ownerDocument.removeEventListener("mousemove",	cXULElement_scale._onDocumentMouseMove,false);
-	oElement.ownerDocument.removeEventListener("mouseup",	cXULElement_scale._onDocumentMouseUp,	false);
-
-	// restore visual state
-	oElement.$setPseudoClass("active", false, "button");
-
-	var oPosition	= oElement.getBoundingClientRect();
-	var sValue	= oElement.attributes["min"] * 1.0 + Math.round((oElement._buttonX  / (oPosition.right - oPosition.left - 17)) * (oElement.attributes["max"] * 1.0 - oElement.attributes["min"] * 1.0) / oElement.attributes["step"]) * oElement.attributes["step"];
-
-	var sValueOld	= oElement.$getValue();
-    oElement.$setValue(sValue);
-	if (sValueOld != oElement.$getValue())
-	{
-	    // Fire Event
-		cXULInputElement.dispatchChange(oElement);
-	}
-};
-
-cXULElement_scale._onDocumentMouseMove     = function(oEvent) {
-	var oElement	= cXULElement_scale._element;
-	var oPosition	= oElement.getBoundingClientRect(),
-		nWidth		= oPosition.right - oPosition.left;
-
-    oElement._buttonX  += oEvent.clientX - oElement._mouseX;
-    oElement._buttonY  += oEvent.clientY - oElement._mouseY;
-
-    oElement.$getContainer("button").style.left   =(oElement._buttonX < 0 ? 0 : oElement._buttonX < nWidth - 17 ? oElement._buttonX : nWidth - 17) + "px";
-
-    oElement._mouseX    = oEvent.clientX;
-    oElement._mouseY    = oEvent.clientY;
-};
-
-// Class Event Handlers
 cXULElement_scale.handlers	= {
-	"focus":	function(oEvent) {
-		this.$getContainer().focus();
+	"mousedown":	function(oEvent) {
+		if (oEvent.$pseudoTarget && oEvent.$pseudoTarget.className.match(/xul-scale--button/)) {
+			cXULElement_scale.startSession(this, oEvent.$pseudoTarget);
+		}
 	},
-	"blur":		function(oEvent) {
-		this.$getContainer().blur();
+	"mouseup":		function(oEvent) {
+		if (cXULElement_scale.captured) {
+			cXULElement_scale.finishSession(this);
+			//
+			if (cXULElement_scale.prevValue != this.getAttribute("value"))
+				cXULInputElement.dispatchChange(this);
+		}
 	},
 	"keydown":	function(oEvent) {
-	    var sValue  = this.$getValue();
-	    if (oEvent.keyIdentifier == "Down" || oEvent.keyIdentifier == "Right") {
-	        this.$setValue(sValue * 1.0 + this.attributes["step"] * 1.0);
-	        oEvent.preventDefault();
-	    }
-	    else
-	    if (oEvent.keyIdentifier == "Up" || oEvent.keyIdentifier == "Left") {
-	        this.$setValue(sValue * 1.0 - this.attributes["step"] * 1.0);
-	        oEvent.preventDefault();
-	    }
+		if (cXULElement_scale.captured && oEvent.keyIdentifier == "Esc") {
+			cXULElement_scale.finishSession(this);
+			this.setAttribute("value", cXULElement_scale.prevValue);
+		}
+	},
+	"mousemove":	function(oEvent) {
+		if (cXULElement_scale.captured) {
+			var oElement= this.$getContainer("bar").getElementsByTagName("div")[cXULElement_scale.button],
+				aValues	= this.getAttribute("value").split(","),
+				nMin	= this.getAttribute("min") * 1,
+				nMax	= this.getAttribute("max") * 1,
+				bOrient	= this.getAttribute("orient") == "vertical",
+				nSize	= bOrient
+							? 100 * (oEvent.clientY - cXULElement_scale.clientRect.top) / (cXULElement_scale.clientRect.bottom - cXULElement_scale.clientRect.top)
+							: 100 * (oEvent.clientX - cXULElement_scale.clientRect.left) / (cXULElement_scale.clientRect.right - cXULElement_scale.clientRect.left),
+				nValue	= nMin + nSize	* (nMax - nMin) / 100;
 
-	    if (sValue != this.$getValue())
-	    {
-		    // Fire Event
-	    	cXULInputElement.dispatchChange(this);
-	    }
+			// Check lower boundary
+			if (cXULElement_scale.button > 0)
+				if (nValue < aValues[cXULElement_scale.button - 1] * 1)
+					nValue = aValues[cXULElement_scale.button - 1] * 1;
+			// Check upper boundary
+			if (cXULElement_scale.button < aValues.length - 1)
+				if (nValue > aValues[cXULElement_scale.button + 1] * 1)
+					nValue = aValues[cXULElement_scale.button + 1] * 1;
+			// Account for min/max
+			nValue	= Math.min(nMax, Math.max(nMin, nValue));
+
+			aValues[cXULElement_scale.button]	= nValue;
+			// Update value
+			this.setAttribute("value", aValues.join(","));
+			// Update position
+			oElement.style[bOrient ? "top" : "left"]	= 100 * (nValue - nMin) / (nMax - nMin) + "%";
+
+			// TODO: Dispatch continuous change event
+		}
+	},
+	"DOMNodeInsertedIntoDocument":	function(oEvent) {
+		cXULElement_scale.redraw(this);
 	},
 	"DOMAttrModified":	function(oEvent) {
 		if (oEvent.target == this) {
 			switch (oEvent.attrName) {
 				case "value":
-					this.$setValue(oEvent.newValue || '');
+					if (!cXULElement_scale.captured)
+						cXULElement_scale.redraw(this);
 					break;
 
 				case "disabled":
 					this.$setPseudoClass("disabled", oEvent.newValue == "true");
-					this.$getContainer("input").disabled = oEvent.newValue == "true";
-					this.$getContainer().disabled	= oEvent.newValue == "true";
 					break;
-
-				case "type":
-					// TODO
-					break;
-
-				case "max":
-					// TODO
-					break;
-
-				case "min":
-					// TODO
-					break;
-
-				case "step":
-					// TODO
-					break;
-
-				default:
-					this.$mapAttribute(oEvent.attrName, oEvent.newValue);
 			}
 		}
-	},
-	"DOMNodeInsertedIntoDocument":	function(oEvent) {
-		/*
-		for (var oElementTemp = this; oElementTemp; oElementTemp = oElementTemp.parentNode)
-			if (oElementTemp instanceof cXHTMLElement_form)
-				break;
-
-		if (oElementTemp)
-		{
-			// Set reference to the form element
-			this.form	= oElementTemp;
-
-			// Add to collection of elements
-			oElementTemp.elements.$add(this);
-	//		oElementTemp.elements[this.attributes["name"]]	= this;
-		}*/
-		this.$setValue(this.attributes["value"]);
-	}/*,
-	"DOMNodeRemovedFromDocument":	function(oEvent) {
-		if (this.form)
-			this.form.elements.$remove(this);
-	}*/
+	}
 };
 
-cXULElement_scale.getLeft	= function(oInstance, sValue) {
-	var nMax	= oInstance.attributes["max"] * 1,
-		nMin	= oInstance.attributes["min"] * 1;
-	return 100 * (sValue - nMin) / (nMax - nMin) + '%';
+// Static methods
+cXULElement_scale.startSession	= function(oInstance, oThumb) {
+	var oRect	= oInstance.getBoundingClientRect();
+	cXULElement_scale.captured	= true;
+	for (var nIndex = 0, aElements = oInstance.$getContainer("bar").getElementsByTagName("div"), oElement; oElement = aElements[nIndex]; nIndex++)
+		if (oElement == oThumb)
+			break;
+	cXULElement_scale.button	= nIndex;
+	cXULElement_scale.clientRect	= oRect;
+	cXULElement_scale.prevValue	= oInstance.getAttribute("value");
+	oInstance.setCapture(true);
+	//
+	oElement.className	+= ' xul-scale--button_active';
+};
+
+cXULElement_scale.finishSession	= function(oInstance) {
+	var oElement	= oInstance.$getContainer("bar").getElementsByTagName("div")[cXULElement_scale.button];
+	cXULElement_scale.captured	= false;
+	cXULElement_scale.button	=-1;
+	cXULElement_scale.clientRect	= null;
+	oInstance.releaseCapture();
+	//
+	oElement.className	= oElement.className.replace(/ xul-scale--button_active/, '');
+};
+
+cXULElement_scale.redraw	= function(oInstance) {
+	var aValue	= oInstance.getAttribute("value").split(","),
+		nMin	= oInstance.getAttribute("min") * 1,
+		nMax	= oInstance.getAttribute("max") * 1,
+		bOrient	= oInstance.getAttribute("orient") == "vertical",
+		aHtml	= [];
+	for (var nIndex = 0, nValue; nIndex < aValue.length; nIndex++) {
+		nValue	= Math.min(nMax, Math.max(nMin, aValue[nIndex]));
+		aHtml.push('<div class="xul-scale--button" style="position:absolute;' + (bOrient ? "top" : "left")+ ':' + (100 * (nValue - nMin) / (nMax - nMin)) + '%" onmouseover="if (ample.$instance(this).$isAccessible()) this.className += \' xul-scale--button_hover\'" onmouseout="if (ample.$instance(this).$isAccessible()) if (!this.className.match(/_disabled/)) this.className = this.className.replace(\' xul-scale--button_hover\', \'\')"></div>');
+	}
+	oInstance.$getContainer("bar").innerHTML	= aHtml.join('');
 };
 
 // Element Render: open
@@ -193,9 +137,7 @@ cXULElement_scale.prototype.$getTagOpen	= function() {
 	return '<div class="xul-scale' + (this.attributes["disabled"] ? " xul-scale_disabled" : '') + (" xul-scale-orient-" +(this.attributes["orient"] == "vertical" ? "vertical" : "horizontal")) + '"' + (this.attributes["style"] ? ' style="' + this.attributes["style"] + '"': '') + '>\
 				<div class="xul-scale--before" style="float:left"></div>\
 				<div class="xul-scale--after" style="float:right"></div>\
-				<div class="xul-scale--bar">\
-					<div class="xul-scale--button" style="position:relative;font-size:1px;" onmouseout="if (!ample.$instance(this).attributes.disabled) ample.$instance(this).$setPseudoClass(\'hover\', false, \'button\')" onmouseover="if (!ample.$instance(this).attributes.disabled) ample.$instance(this).$setPseudoClass(\'hover\', true, \'button\')" onmousedown="if (!ample.$instance(this).attributes.disabled) {return ample.$instance(this)._onButtonMouseDown(event);}"></div>\
-				</div>\
+				<div class="xul-scale--bar" onmousedown="return false" style="position:relative"></div>\
 				<input type="text" value="' + this.attributes["value"] + '" autocomplete="off" style="display:none;width:1px;height:1px;" class="xul-scale--input"/>\
 			</div>';
 };
