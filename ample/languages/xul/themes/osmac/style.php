61e596748bacc186d5a7deda911288f903397e2a
ilinsky <sergey@ilinsky.com>
2010-07-15 12:32:28 +0200

    Added new themes folders

---
+++ ample/languages/xul/themes/osmac/style.php
@@ -0,0 +1,12 @@
+<?
+	$aFiles		= array();
+	//
+
+    $sOutput	= "@namespace xul \"http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul\";\n\n";
+    for ($nIndex = 0; $nIndex < count($aFiles); $nIndex++)
+        $sOutput	.= join('', file($aFiles[$nIndex])) . "\n";
+
+	header("Content-type: text/css");
+
+	echo $sOutput;
+?>
