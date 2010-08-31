efe0f77885d713d22c85b5d1b769e40c96b751c8
ilinsky <sergey@ilinsky.com>
2010-09-01 00:21:53 +0200

    Implemented forms API

--- ample/plugins/form/form.js
+++ ample/plugins/form/form.js
@@ -7,3 +7,80 @@
  *
  */
 
+ample.extend({
+	//
+	val:	function(vValue) {
+		// validate API
+		ample.guard([
+			["value", Object, true]
+		]);
+
+		// Invoke Implementation
+	},
+
+	//
+	blur:	function(fHandler) {
+		// validate API
+		ample.guard([
+			["handler", Function, true]
+		]);
+
+		// Invoke Implementation
+	},
+
+	focus:	function(fHandler) {
+		// validate API
+		ample.guard([
+			["handler", Function, true]
+		]);
+
+		// Invoke Implementation
+	},
+
+	//
+	change:	function(fHandler) {
+		// validate API
+		ample.guard([
+			["handler", Function, true]
+		]);
+
+		// Invoke Implementation
+	},
+
+	select:	function(fHandler) {
+		// validate API
+		ample.guard([
+			["handler", Function, true]
+		]);
+
+		// Invoke Implementation
+	},
+
+	//
+	submit:	function(fHandler) {
+		// validate API
+		ample.guard([
+			["handler", Function, true]
+		]);
+
+		// Invoke Implementation
+	},
+
+	reset:	function(fHandler) {
+		// validate API
+		ample.guard([
+			["handler", Function, true]
+		]);
+
+		// Invoke Implementation
+	},
+
+	//
+	serialize:	function() {
+
+	},
+
+	serializeArray:	function() {
+
+	}
+});
