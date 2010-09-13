56061d3d6ef2974a18c16cadd91f93331ed111a1
ilinsky <sergey@ilinsky.com>
2010-09-13 18:33:37 +0200

    Implemented dimensions module

--- ample/runtime/modules/dimensions.js
+++ ample/runtime/modules/dimensions.js
@@ -49,37 +49,61 @@ cAMLQuery.prototype.height	= function(sValue) {
 };
 
 cAMLQuery.prototype.innerWidth	= function() {
-	// Validate API call
-	fGuard(arguments, [
-		["value",	cObject, true]
-	]);
-
-	return this;
+	if (this.length) {
+		var oRect	= fAMLElement_getBoundingClientRect(this[0]),
+			oElementDOM	= this[0].$getContainer(),
+			nPaddingLeft	= fParseInt(fBrowser_getStyle(oElementDOM, "paddingLeft")),
+			nPaddingRight	= fParseInt(fBrowser_getStyle(oElementDOM, "paddingRight"));
+		return (oRect.right - oRect.left) +
+				(fIsNaN(nPaddingLeft) ? 0 : nPaddingLeft) +	(fIsNaN(nPaddingRight) ? 0 : nPaddingRight);
+	}
+	return 0;
 };
 
 cAMLQuery.prototype.innerHeight	= function() {
-	// Validate API call
-	fGuard(arguments, [
-		["value",	cObject, true]
-	]);
-
-	return this;
+	if (this.length) {
+		var oRect	= fAMLElement_getBoundingClientRect(this[0]),
+			oElementDOM	= this[0].$getContainer(),
+			nPaddingTop		= fParseInt(fBrowser_getStyle(oElementDOM, "paddingTop")),
+			nPaddingBottom	= fParseInt(fBrowser_getStyle(oElementDOM, "paddingBottom"));
+		return (oRect.bottom - oRect.top) +
+				(fIsNaN(nPaddingTop) ? 0 : nPaddingTop) + (fIsNaN(nPaddingBottom) ? 0 : nPaddingBottom);
+	}
+	return 0;
 };
 
-cAMLQuery.prototype.outerWidth	= function() {
-	// Validate API call
-	fGuard(arguments, [
-		["value",	cObject, true]
-	]);
-
-	return this;
+cAMLQuery.prototype.outerWidth	= function(bMargin) {
+	if (this.length) {
+		var oRect	= fAMLElement_getBoundingClientRect(this[0]),
+			oElementDOM	= this[0].$getContainer(),
+			nPaddingLeft	= fParseInt(fBrowser_getStyle(oElementDOM, "paddingLeft")),
+			nPaddingRight	= fParseInt(fBrowser_getStyle(oElementDOM, "paddingRight")),
+			nMarginLeft		= fParseInt(fBrowser_getStyle(oElementDOM, "marginLeft")),
+			nMarginRight	= fParseInt(fBrowser_getStyle(oElementDOM, "marginRight")),
+			nBorderLeft		= fParseInt(fBrowser_getStyle(oElementDOM, "borderLeftWidth")),
+			nBorderRight	= fParseInt(fBrowser_getStyle(oElementDOM, "borderRightWidth"));
+		return (oRect.right - oRect.left) +
+				(fIsNaN(nPaddingLeft) ? 0 : nPaddingLeft) + (fIsNaN(nPaddingRight) ? 0 : nPaddingRight) +
+				(fIsNaN(nBorderLeft) ? 0 : nBorderLeft) + (fIsNaN(nBorderRight) ? 0 : nBorderRight) +
+				(bMargin ? ((fIsNaN(nMarginLeft) ? 0 : nMarginLeft) + (fIsNaN(nMarginRight) ? 0 : nMarginRight)) : 0);
+	}
+	return 0;
 };
 
-cAMLQuery.prototype.outerHeight	= function() {
-	// Validate API call
-	fGuard(arguments, [
-		["value",	cObject, true]
-	]);
-
-	return this;
+cAMLQuery.prototype.outerHeight	= function(bMargin) {
+	if (this.length) {
+		var oRect	= fAMLElement_getBoundingClientRect(this[0]),
+			oElementDOM	= this[0].$getContainer(),
+			nPaddingTop		= fParseInt(fBrowser_getStyle(oElementDOM, "paddingTop")),
+			nPaddingBottom	= fParseInt(fBrowser_getStyle(oElementDOM, "paddingBottom")),
+			nMarginTop		= fParseInt(fBrowser_getStyle(oElementDOM, "marginTop")),
+			nMarginBottom	= fParseInt(fBrowser_getStyle(oElementDOM, "marginBottom")),
+			nBorderTop		= fParseInt(fBrowser_getStyle(oElementDOM, "borderTopWidth")),
+			nBorderBottom	= fParseInt(fBrowser_getStyle(oElementDOM, "borderBottomWidth"));
+		return (oRect.bottom - oRect.top) +
+				(fIsNaN(nPaddingTop) ? 0 : nPaddingTop) + (fIsNaN(nPaddingBottom) ? 0 : nPaddingBottom) +
+				(fIsNaN(nBorderTop) ? 0 : nBorderTop) + (fIsNaN(nBorderBottom) ? 0 : nBorderBottom) +
+				(bMargin ? ((fIsNaN(nMarginTop) ? 0 : nMarginTop) + (fIsNaN(nMarginBottom) ? 0 : nMarginBottom)) : 0);
+	}
+	return 0;
 };
