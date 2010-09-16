17341aa475c1ec9c3ce71771f17cec2181372bbd
ilinsky <sergey@ilinsky.com>
2010-09-16 17:08:47 +0200

    Removed data load method implementation

--- ample/runtime/processors/aml/elements/data.js
+++ ample/runtime/processors/aml/elements/data.js
@@ -16,52 +16,6 @@ cAMLElement_data.prototype	= new cAMLElement_prototype("data");
 cAMLElement_data.prototype.customers	= null;
 
 // Public Methods
-cAMLElement_data.prototype.load	= function(sUrl, bAsync) {
-	// Clean up content
-	if (this.firstChild) {
-		// Dispatch unload event
-		var oEvent	= new cAMLEvent;
-		oEvent.initEvent("unload", false, false);
-		fAMLNode_dispatchEvent(this, oEvent);
-
-		// Clear document
-		while (this.firstChild)
-			fAMLElement_removeChild(this, this.firstChild);
-	}
-
-	var oRequest	= new cXMLHttpRequest;
-	var oElement	= this;
-	function fOnLoad() {
-		var oDocument	= fBrowser_getResponseDocument(oRequest);
-		if (oDocument) {
-			// process response
-			fAMLElement_appendChild(oElement, fAMLDocument_importNode(oElement.ownerDocument, oDocument.documentElement, true));
-
-			// Dispatch load event
-			var oEvent	= new cAMLEvent;
-			oEvent.initEvent("load", false, false);
-			fAMLNode_dispatchEvent(oElement, oEvent);
-		}
-		else {
-			// Dispatch error event
-			var oEvent	= new cAMLEvent;
-			oEvent.initEvent("error", true, false);
-			fAMLNode_dispatchEvent(oElement, oEvent);
-		}
-	};
-
-	// Make request for the new content
-	oRequest.open("GET", sUrl, bAsync);
-	if (bAsync)
-		oRequest.onreadystatechange	= function() {
-			if (oRequest.readyState == 4)
-				fOnLoad();
-		};
-	oRequest.send(null);
-	if (!bAsync)
-		fOnLoad();
-};
-
 cAMLElement_data.prototype.register	= function(oElement) {
 	this.customers.$add(oElement);
 };
@@ -79,7 +33,7 @@ cAMLElement_data.prototype.notify	= function() {
 cAMLElement_data.handlers	= {};
 cAMLElement_data.handlers["DOMNodeInsertedIntoDocument"]	= function(oEvent) {
 	if (this.attributes["src"])
-		this.load(this.attributes["src"], this.attributes["async"] != "false");
+		fAMLQuery_load_start(this, this.attributes["src"]);
 };
 cAMLElement_data.handlers["DOMNodeRemovedFromDocument"]	= function(oEvent) {
 	// TODO: unregister customers
@@ -103,7 +57,7 @@ cAMLElement_data.handlers["DOMAttrModified"]	= function(oEvent) {
 	if (oEvent.target == this)
 		switch (oEvent.newValue) {
 			case "src":
-				this.load(sValue, this.attributes["async"] != "false");
+				fAMLQuery_load_start(this, oEvent.newValue);
 				break;
 
 			case "type":
