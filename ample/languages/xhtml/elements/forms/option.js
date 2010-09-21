f904f97a58403ea0dc18c24a7588804db708a09b
ilinsky <sergey@ilinsky.com>
2010-09-21 13:21:12 +0200

    Implemented selection for select-single

--- ample/languages/xhtml/elements/forms/option.js
+++ ample/languages/xhtml/elements/forms/option.js
@@ -12,11 +12,35 @@ cXHTMLElement_option.prototype	= new cXHTMLElement("option");
 cXHTMLElement_option.prototype.$selectable	= false;
 cXHTMLElement_option.prototype.$hoverable	= true;
 
+// Static Methods
+cXHTMLElement_option.ensureRowIsVisible	= function(oInstance) {
+	for (var oElement = oInstance; oElement = oElement.parentNode;)
+		if (oElement instanceof cXHTMLElement_select) {
+			var oScroll	= oElement.$getContainer(),
+				oInput	= oInstance.$getContainer(),
+				nDiffTop	= oInput.offsetTop - oScroll.offsetTop,
+				nDiffHeight	= oInput.offsetHeight - oScroll.offsetHeight;
+			if (oScroll.scrollTop < nDiffTop + nDiffHeight)
+				oScroll.scrollTop	= nDiffTop + nDiffHeight;
+			else
+			if (oScroll.scrollTop > nDiffTop)
+				oScroll.scrollTop	= nDiffTop;
+			break;
+		}
+};
+
 // Class Events Handlers
 cXHTMLElement_option.handlers	= {
 	"DOMAttrModified":	function(oEvent) {
 		if (oEvent.target == this)
-			cXHTMLElement.mapAttribute(this, oEvent.attrName, oEvent.newValue);
+			switch (oEvent.attrName) {
+				case "selected":
+					this.$setPseudoClass("selected", oEvent.newValue != null && oEvent.newValue != "false");
+					break;
+
+				default:
+					cXHTMLElement.mapAttribute(this, oEvent.attrName, oEvent.newValue);
+			}
 	}
 };
 
--- ample/languages/xhtml/elements/forms/select.js
+++ ample/languages/xhtml/elements/forms/select.js
@@ -20,6 +20,7 @@ cXHTMLElement_select.prototype	= new cXHTMLInputElement("select");
 
 // Public Properties
 cXHTMLElement_select.prototype.options	= null;
+cXHTMLElement_select.prototype.selectedIndex	=-1;
 
 // Public Methods
 cXHTMLElement_select.prototype.add		= function(oElement, nIndex) {
@@ -37,6 +38,53 @@ cXHTMLElement_select.handlers	= {
 	},
 	"blur":		function(oEvent) {
 		this.$getContainer("value").blur();
+	},
+	"keydown":	function(oEvent) {
+		if (oEvent.keyIdentifier == "Up") {
+			// Deselect previously selected item
+			if (this.selectedIndex !=-1)
+				this.options[this.selectedIndex].removeAttribute("selected");
+			// Find new item
+			if (this.selectedIndex > 0)
+				this.selectedIndex--;
+			else
+			if (this.options.length)
+				this.selectedIndex	= this.options.length - 1;
+			// Select new item
+			if (this.selectedIndex >-1) {
+				this.options[this.selectedIndex].setAttribute("selected", "true");
+				cXHTMLElement_option.ensureRowIsVisible(this.options[this.selectedIndex]);
+			}
+			// Prevent scrolling
+			oEvent.preventDefault();
+		}
+		else
+		if (oEvent.keyIdentifier == "Down") {
+			// Deselect previously selected item
+			if (this.selectedIndex !=-1)
+				this.options[this.selectedIndex].removeAttribute("selected");
+			// Find new item
+			if (this.selectedIndex < this.options.length - 1)
+				this.selectedIndex++;
+			else
+			if (this.options.length)
+				this.selectedIndex	= 0;
+			// Select new item
+			if (this.selectedIndex >-1) {
+				this.options[this.selectedIndex].setAttribute("selected", "true");
+				cXHTMLElement_option.ensureRowIsVisible(this.options[this.selectedIndex]);
+			}
+			// Prevent scrolling
+			oEvent.preventDefault();
+		}
+	},
+	"click":	function(oEvent) {
+		if (oEvent.target instanceof cXHTMLElement_option && this.options[this.selectedIndex] != oEvent.target) {
+			if (this.selectedIndex >-1)
+				this.options[this.selectedIndex].removeAttribute("selected");
+			this.options[this.selectedIndex = this.options.$indexOf(oEvent.target)].setAttribute("selected", "true");
+			cXHTMLElement_option.ensureRowIsVisible(this.options[this.selectedIndex]);
+		}
 	},
 	"DOMNodeInserted":	function(oEvent) {
 		if (oEvent.target instanceof cXHTMLElement_option)
