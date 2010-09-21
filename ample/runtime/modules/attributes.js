9599b7a9f3da978e6dd7a374e4ca24ce021669a2
ilinsky <sergey@ilinsky.com>
2010-09-21 12:58:42 +0200

    Removing attributes fixed

--- ample/runtime/modules/attributes.js
+++ ample/runtime/modules/attributes.js
@@ -8,25 +8,28 @@
  */
 
 
-cAMLQuery.prototype.attr	= function(sName, sValue) {
+cAMLQuery.prototype.attr	= function(sQName, sValue) {
 	// Validate API call
 	fGuard(arguments, [
 		["name",	cString],
-		["value",	cObject, true]
+		["value",	cObject,	true,	true]
 	]);
 
 	// Invoke implementation
 	if (arguments.length > 1) {
-		var aQName		= sName.split(':'),
+		var aQName		= sQName.split(':'),
+			sLocalName	= aQName.pop(),
+			sPrefix		= aQName.pop() || null,
 			sNameSpaceURI	= null;
-		if (aQName.length > 1)
-			sNameSpaceURI	=(this.resolver || fAmple_resolver)(aQName[0]) || null;
+		if (sPrefix != null)
+			sNameSpaceURI	=(this.resolver || fAmple_resolver)(sPrefix) || null;
 		fAMLQuery_each(this, function() {
-			fAMLElement_setAttributeNS(this, sNameSpaceURI, sName, cString(sValue));
+			sValue == null ? fAMLElement_removeAttributeNS(this, sNameSpaceURI, sLocalName) : fAMLElement_setAttributeNS(this, sNameSpaceURI, sQName, cString(sValue));
 		});
 		return this;
 	}
 	else
 	if (this.length)
-		return fAMLElement_getAttribute(this[0], sName);
+		return fAMLElement_getAttribute(this[0], sQName);
+	return null;
 };
