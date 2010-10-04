848bccbcc51293f555be8408595bd678d78f7d91
ilinsky <sergey@ilinsky.com>
2010-10-04 19:19:30 +0200

    Created element files

---
+++ ample/languages/xmlschema/elements/schema.js
@@ -0,0 +1,14 @@
+/*
+ * Ample SDK - JavaScript GUI Framework
+ *
+ * Copyright (c) 2010 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ * See: http://www.amplesdk.com/about/licensing/
+ *
+ */
+
+var cXSElement_schema	= function(){};
+cXSElement_schema.prototype    = new cXSNamespaceItem;
+
+// Register Element
+ample.extend(cXSElement_schema);
