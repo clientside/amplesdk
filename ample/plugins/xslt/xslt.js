5e121257d2d8344f6b83e50a24a1cb1350a622c9
ilinsky <sergey@ilinsky.com>
2010-10-13 17:17:59 +0200

    Initialized XSLT plugin

---
+++ ample/plugins/xslt/xslt.js
@@ -0,0 +1,41 @@
+/*
+ * Ample SDK - JavaScript GUI Framework
+ *
+ * Copyright (c) 2010 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ * See: http://www.amplesdk.com/about/licensing/
+ *
+ */
+
+(function() {
+	function fAmple_transform(oXml, oXsl) {
+
+	};
+
+	// Extend ample object
+	ample.extend({
+		xslt:	function(vXml, vXsl, fCallback) {
+			// validate API
+			ample.guard(arguments, [
+				["xml", 	Object],
+				["xsl", 	Object],
+				["callback",Function,	true]
+			]);
+
+			// Invoke Implementation
+		}
+	});
+
+	// Extend collection object
+	ample.extend({
+		xslt:	function(vXml, vXsl) {
+			// validate API
+			ample.guard(arguments, [
+				["xml", 	Object],
+				["xsl", 	Object]
+			]);
+
+			// Invoke Implementation
+		}
+	}, AMLQuery.prototype);
+})();
