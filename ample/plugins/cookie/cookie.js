e9045e24752802885ead9faab35370253573834e
ilinsky <sergey@ilinsky.com>
2010-10-14 18:18:46 +0200

    Implemented cookie plugin

--- ample/plugins/cookie/cookie.js
+++ ample/plugins/cookie/cookie.js
@@ -8,21 +8,43 @@
  */
 
 // Private members
-function fAmple_cookie(oAmple, sName, vValue) {
+function fAmple_setCookie(oAmple, sName, vValue, oSettings) {
+	if (vValue == null) {
+		oSettings.expires	=-1;
+		vValue	= '';
+	}
+	document.cookie	= sName + '=' + encodeURIComponent(vValue) +
+									(oSettings.expires	? '; expires=' + (oSettings.expires instanceof Date ? oSettings.expires : new Date(new Date().getTime() + Number(oSettings.expires) * 864e5)).toUTCString() : '') +
+									(oSettings.path		? '; path=' + oSettings.path : '') +
+									(oSettings.domain	? '; domain=' + oSettings.domain : '') +
+									(oSettings.secure	? '; secure' : '');
+};
 
+function fAmple_getCookie(oAmple, sName) {
+	var oCookie	= document.cookie;
+	if (oCookie && oCookie != '') {
+		for (var nIndex = 0, aCookie = oCookie.split(';'), nLength = aCookie.length, sCookie; nIndex < nLength; nIndex++)
+			if (aCookie[nIndex].match(/^\s*([^=]+)=(.*)\s*/) && RegExp.$1 == sName)
+				return decodeURIComponent(RegExp.$2);
+	}
+	return null;
 };
 
 // Extend ample object
 ample.extend({
 	//
-	cookie:	function(sName, vValue) {
+	cookie:	function(sName, vValue, oSettings) {
 		// Validate API call
 		ample.guard(arguments, [
-			["name",	cString],
-			["value",	cObject, true, true]
+			["name",	String],
+			["value",	Object, true, true],
+			["settings",Object,	true]
 		]);
 
 		// Invoke implementation
-		return fAmple_cookie(this, sName, vValue);
+		if (arguments.length > 1)
+			fAmple_setCookie(this, sName, vValue, oSettings || {});
+		else
+			return fAmple_getCookie(this, sName);
 	}
 });
