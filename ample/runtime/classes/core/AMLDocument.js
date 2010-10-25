clientside/amplesdk

commit f477a3a8bb628affc89499afac5f375ab2b94d46

Fixed issue 13 - XUL examples do not work in Firefox 4b6

ample/runtime/classes/core/AMLDocument.js
@@ -332,8 +332,14 @@ cAMLDocument.prototype.getElementsByTagNameNS	= function(sNameSpaceURI, sLocalNa
 function fAMLDocument_importNode(oDocument, oElementDOM, bDeep, oNode, bCollapse) {
 	switch (oElementDOM.nodeType) {
 		case cAMLNode.ELEMENT_NODE:
-			var sNameSpaceURI	= oElementDOM.namespaceURI || (bTrident
-					?(function (oNode/*, sPrefix*/) {
+			var sNameSpaceURI	= oElementDOM.namespaceURI,
+				sLocalName		= oElementDOM.localName || oElementDOM.baseName;
+			// Bugfix FF4 (remote XUL)
+			if (bGecko && sNameSpaceURI == sNS_XUL + '#')
+				sNameSpaceURI	= sNS_XUL;
+			if (bTrident)
+				sNameSpaceURI	=
+					(function (oNode/*, sPrefix*/) {
 						// Lookup entity reference node
 						while (oNode = oNode.parentNode)
 							if (oNode.nodeType == cAMLNode.ENTITY_REFERENCE_NODE)
@@ -353,9 +359,7 @@ function fAMLDocument_importNode(oDocument, oElementDOM, bDeep, oNode, bCollapse
 										return oNode.attributes[nIndex].value;
 */
 						return null;
-					})(oElementDOM/*, oElementDOM.prefix*/)
-					: null),
-				sLocalName		= oElementDOM.localName || oElementDOM.baseName;
+					})(oElementDOM/*, oElementDOM.prefix*/);
 			// XInclude 1.0
 			if (sNameSpaceURI == sNS_XINCLUDE) {
 				if (sLocalName == "include") {

ample/runtime/ample.js
@@ -20,13 +20,17 @@ function fQuery(vArgument1, vArgument2, vArgument3) {
 		if (typeof vArgument1 == "string" || vArgument1 instanceof cString) {
 			if (vArgument1.substr(0,1) == '<') {
 				// XML string
-				var aNameSpaces	= [];
+				var aNameSpaces	= [],
+					sText;
 				for (var sKey in oAmple.prefixes)
 					if (oAmple.prefixes.hasOwnProperty(sKey) && sKey != "toString")
 						aNameSpaces.push("xmlns" + (sKey == '' ? '' : ':') + sKey + '="' + oAmple.prefixes[sKey] + '"');
 				//
-				var oDocument	= new cDOMParser().parseFromString(
+				sText	=						//		"<?" + "xml" + ' ' + 'version="1.0"' + "?>" +
 														'<!' + "DOCTYPE" + ' ' + "div" + '[' + aUtilities_entities + ']>' +
+//->Debug
+														'\n' +
+//<-Debug
 														'<' + "div" + ' ' + "type" + '="' + "application/ample+xml" + '"' + ' ' + aNameSpaces.join(' ') + '>' +
 //->Debug
 														'\n' +
@@ -35,7 +39,12 @@ function fQuery(vArgument1, vArgument2, vArgument3) {
 //->Debug
 														'\n' +
 //<-Debug
-														'</' + "div" + '>', "text/xml");
+														'</' + "div" + '>';
+				// Bugfix FF4 (remote XUL)
+				if (bGecko)
+					sText	= sText.replace(new cRegExp(sNS_XUL, 'g'), sNS_XUL + '#');
+				//
+				var oDocument	= new cDOMParser().parseFromString(sText, "text/xml");
 				if (!oDocument || ((bTrident && oDocument.parseError != 0) || !oDocument.documentElement || oDocument.getElementsByTagName("parsererror").length))
 					throw new cAMLException(cAMLException.SYNTAX_ERR, fQuery.caller);
 				else

ample/runtime/browser.js
@@ -715,8 +715,17 @@ function fBrowser_getResponseDocument(oRequest) {
 	var oDocument	= oRequest.responseXML,
 		sText		= oRequest.responseText;
 	// Try parsing responseText
-	if (bTrident && sText && oDocument && !oDocument.documentElement && oRequest.getResponseHeader("Content-Type").match(/[^\/]+\/[^\+]+\+xml/))
-		oDocument	= new cDOMParser().parseFromString(sText, "text/xml");
+	if (sText) {
+		if (bTrident) {
+			if (oDocument && !oDocument.documentElement && oRequest.getResponseHeader("Content-Type").match(/[^\/]+\/[^\+]+\+xml/))
+				oDocument	= new cDOMParser().parseFromString(sText, "text/xml");
+		}
+		// Bugfix FF4 (remote XUL)
+		if (bGecko) {
+			sText	= sText.replace(new cRegExp(sNS_XUL, 'g'), sNS_XUL + '#');
+			oDocument	= new cDOMParser().parseFromString(sText, "text/xml");
+		}
+	}
 	// Check if there is no error in document
 	if (!oDocument || ((bTrident && oDocument.parseError != 0) || !oDocument.documentElement || oDocument.getElementsByTagName("parsererror").length))
 		return null;

ample/runtime/utilities.js
@@ -149,7 +149,7 @@ function fAML_processScripts() {
 
 	// Process script tags
     aElements = oBrowser_body.getElementsByTagName("script");
-    for (var nIndex = 0, nSkip = 0; aElements.length > nSkip; nIndex++) {
+    for (var nIndex = 0, nSkip = 0, sText; aElements.length > nSkip; nIndex++) {
     	// Current Script
 	    oElementDOM	= aElements[nSkip];
 
@@ -186,8 +186,7 @@ function fAML_processScripts() {
 				bReferenced	= true;
 			}
 			else {
-				// Create fragment
-			    oDocument   = new cDOMParser().parseFromString(//		"<?" + "xml" + ' ' + 'version="1.0"' + "?>" +
+				sText	=										//		"<?" + "xml" + ' ' + 'version="1.0"' + "?>" +
 																		'<!' + "DOCTYPE" + ' ' + "div" + '[' + aUtilities_entities + ']>' +
 //->Debug
 																		'\n' +
@@ -200,7 +199,12 @@ function fAML_processScripts() {
 //->Debug
 			    														'\n' +
 //<-Debug
-			    														'</' + "div" + '>', "text/xml");
+			    														'</' + "div" + '>';
+				// Bugfix FF4 (remote XUL)
+				if (bGecko)
+					sText	= sText.replace(new cRegExp(sNS_XUL, 'g'), sNS_XUL + '#');
+				// Create fragment
+			    oDocument   = new cDOMParser().parseFromString(sText, "text/xml");
 			}
 
 			oParserError	= oDocument ? oDocument.getElementsByTagName("parsererror")[0] : null;

commit c494ca75e1eab337c526e5ac84f24c2d23d3fce8

Fixed IE issue caused in commit f477a3a

ample/runtime/classes/core/AMLDocument.js
@@ -337,7 +337,7 @@ function fAMLDocument_importNode(oDocument, oElementDOM, bDeep, oNode, bCollapse
 			// Bugfix FF4 (remote XUL)
 			if (bGecko && sNameSpaceURI == sNS_XUL + '#')
 				sNameSpaceURI	= sNS_XUL;
-			if (bTrident)
+			if (bTrident && !sNameSpaceURI)
 				sNameSpaceURI	=
 					(function (oNode/*, sPrefix*/) {
 						// Lookup entity reference node