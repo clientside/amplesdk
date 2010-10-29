4bf31969f091f0f49eda274baf081e20ffb5ea81
ilinsky <sergey@ilinsky.com>
2010-10-29 16:34:01 +0200

    Added support for style attribute on description element

--- ample/languages/xul/elements/description.js
+++ ample/languages/xul/elements/description.js
@@ -28,7 +28,10 @@ cXULElement_description.handlers	= {
 
 // Element Render: open
 cXULElement_description.prototype.$getTagOpen		= function() {
-    return '<div class="xul-description' +(this.attributes["class"] ? " " + this.attributes["class"] : '')+ '" style="width:100%;height:100%;">' + (this.attributes["value"] || "");
+    return '<div class="xul-description' +(this.attributes["class"] ? " " + this.attributes["class"] : '')+ '" style="' +
+    			'width:100%;height:100%;'+
+    			(this.attributes["style"] ? this.attributes["style"] : '') +
+    			'">' + (this.attributes["value"] || "");
 };
 
 // Element Render: close
