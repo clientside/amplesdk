eaad806dc3bf9df048312e051d2b7b46c811dcde
ilinsky <sergey@ilinsky.com>
2010-11-08 23:44:55 +0100

    Implemented shared windowed behavior - opacity effect and content hiding while interacting

--- ample/languages/xul/classes/cXULWindowElement.js
+++ ample/languages/xul/classes/cXULWindowElement.js
@@ -16,5 +16,34 @@ cXULWindowElement.prototype.localName	= "#element-window";
 cXULWindowElement.prototype.$draggable	= true;
 cXULWindowElement.prototype.$resizable	= true;
 
+cXULWindowElement.prototype.open	= function() {
+	// TODO
+};
+
+cXULWindowElement.prototype.close	= function() {
+	// TODO
+};
+
+//
+cXULWindowElement.interactionstart	= function(oEvent) {
+	var that	= oEvent.currentTarget;
+	if (oEvent.target == that) {
+		if (oEvent.type == "dragstart" && oEvent.$pseudoTarget != that.$getContainer("title"))
+			oEvent.preventDefault();
+		else {
+			that.$getContainer("body").style.visibility	= "hidden";
+			ample.query(that).animate({opacity:0.75});
+		}
+	}
+};
+
+cXULWindowElement.interactionend	= function(oEvent) {
+	var that	= oEvent.currentTarget;
+	if (oEvent.target == that) {
+		that.$getContainer("body").style.visibility	= "";
+		ample.query(that).animate({opacity:1.0});
+	}
+};
+
 // Register Element
 ample.extend(cXULWindowElement);
