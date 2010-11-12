af9065f4fb9f70acaaeeb119917f4c1e0120a83c
ilinsky <sergey@ilinsky.com>
2010-11-12 21:28:22 +0100

    Issue 43 - active/hover visual state persists on elements that open modal dialogs

--- ample/runtime/modules/modal.js
+++ ample/runtime/modules/modal.js
@@ -7,14 +7,30 @@
  *
  */
 
-oAmple.modal		= function(oElement) {
+oAmple.modal		= function(oNode) {
 	// Validate arguments
 	fGuard(arguments, [
 		["element",	cAMLElement, true, true]
 	]);
 	//
-	if (oElement == null)
+	if (oNode == null) {
 		oBrowser_modalNode	= oAmple.activeElement	= null;
-	else
-		oBrowser_modalNode	= oAmple.activeElement	= oElement;
+	}
+	else {
+		// Propagate mouseleave
+		for (var nIndex = 0, oElement, oEvent; nIndex < aBrowser_mouseNodes.length - 1; nIndex++) {
+			oElement	= aBrowser_mouseNodes[nIndex];
+		    // Remove :hover pseudo-class
+			if (oElement.$hoverable && oElement.$isAccessible())
+				fAMLElement_setPseudoClass(oElement, "hover", false);
+			//
+			oEvent = new cAMLMouseEvent;
+			oEvent.initMouseEvent("mouseleave", false, false, window, null, 0, 0, 0, 0, false, false, false, false, 0, aBrowser_mouseNodes[nIndex + 1] || null);
+			oEvent.$pseudoTarget	= oElement.$getContainer();
+		    fAMLNode_dispatchEvent(oElement, oEvent);
+		}
+		aBrowser_mouseNodes	= new cAMLNodeList;
+		//
+		oBrowser_modalNode	= oAmple.activeElement	= oNode;
+	}
 };
