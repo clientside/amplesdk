e89c3fe612b8993959ff8c0bb737bbd3a047eb43
ilinsky <sergey@ilinsky.com>
2010-11-17 23:57:27 +0100

    Removed overflow:auto from page

---
+++ ample/languages/xul/elements/sidebar.js
@@ -0,0 +1,32 @@
+/*
+ * Ample SDK - JavaScript GUI Framework
+ *
+ * Copyright (c) 2009 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ * See: http://www.amplesdk.com/about/licensing/
+ *
+ */
+
+var cXULElement_sidebar	= function(){};
+cXULElement_sidebar.prototype	= new cXULElement("sidebar");
+cXULElement_sidebar.prototype.viewType	= cXULElement.VIEW_TYPE_BOXED;
+
+// Attributes Defaults
+cXULElement_sidebar.attributes	= {};
+cXULElement_sidebar.attributes.orient	= "vertical";
+cXULElement_sidebar.attributes.width	= "100%";
+cXULElement_sidebar.attributes.height	= "100%";
+
+// Element Renders
+cXULElement_sidebar.prototype.$getTagOpen	= function() {
+    return '<div class="xul-sidebar' + (this.attributes["class"] ? " " + this.attributes["class"] : "") + '" style="' +
+    			(this.attributes["style"] ? this.attributes["style"] : '') + '">';
+};
+
+// Element Render: close
+cXULElement_sidebar.prototype.$getTagClose	= function() {
+    return '</div>';
+};
+
+// Register Element
+ample.extend(cXULElement_sidebar);
