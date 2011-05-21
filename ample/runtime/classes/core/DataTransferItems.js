fcc230ccc5bd06551904c68bc7cc65cefc2cb97e
ilinsky <sergey@ilinsky.com>
2011-05-21 23:19:23 +0200

    Added DataTransferItems.js

---
+++ ample/runtime/classes/core/DataTransferItems.js
@@ -0,0 +1,9 @@
+/*
+ * Ample SDK - JavaScript GUI Framework
+ *
+ * Copyright (c) 2011 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ * See: http://www.amplesdk.com/about/licensing/
+ *
+ */
+
