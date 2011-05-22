3a409ec352d69cf82f787fc282a0910edf86d8df
ilinsky <sergey@ilinsky.com>
2011-05-22 18:58:02 +0200

    Added queue stub, implementation pending

---
+++ ample/runtime/modules/queue.js
@@ -0,0 +1,28 @@
+/*
+ * Ample SDK - JavaScript GUI Framework
+ *
+ * Copyright (c) 2011 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ * See: http://www.amplesdk.com/about/licensing/
+ *
+ */
+
+oAmple.queue	= function(oElement, sType, vData) {
+	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+};
+
+oAmple.dequeue	= function(oElement, sType) {
+	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+};
+
+cQuery.prototype.queue	= function(sType, vData) {
+	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+};
+
+cQuery.prototype.dequeue	= function(sType) {
+	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+};
+
+cQuery.prototype.delay	= function(nDelay, sType) {
+	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+};
