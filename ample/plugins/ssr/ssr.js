63f967b55a38d922fab44f086d23eecdb51962a2
ilinsky <sergey@ilinsky.com>
2011-06-01 21:52:34 +0200

    Added server-side rendering plugin

---
+++ ample/plugins/ssr/ssr.js
@@ -0,0 +1,23 @@
+/*
+ * Ample SDK - JavaScript GUI Framework
+ *
+ * Copyright (c) 2011 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ * See: http://www.amplesdk.com/about/licensing/
+ *
+ */
+
+function fSsr_import(oElement) {
+	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+};
+
+ample.extend(ample.classes.Query.prototype, {
+	import:	function(oElement) {
+		// Validate API call
+		ample.guard(arguments, [
+			["element",	cElement]
+		]);
+
+		return fSsr_import(oElement);
+	}
+});
