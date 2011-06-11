ed901ee614065528445c09da57c787839d2d2bac
ilinsky <sergey@ilinsky.com>
2011-06-11 21:41:45 +0200

    Skeleton for touch plugin

---
+++ ample/plugins/touch/touch.js
@@ -0,0 +1,29 @@
+/*
+ * Ample SDK - JavaScript GUI Framework
+ *
+ * Copyright (c) 2011 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ * See: http://www.amplesdk.com/about/licensing/
+ *
+ */
+
+function fTouch_onTouchStart(oEvent) {
+
+};
+
+function fTouch_onTouchMove(oEvent) {
+
+};
+
+function fTouch_onTouchEnd(oEvent) {
+
+};
+
+function fTouch_onTouchCancel(oEvent) {
+
+};
+
+ample.addEventListener("touchstart",	fTouch_onTouchStart,	false);
+ample.addEventListener("touchmove",		fTouch_onTouchMove,		false);
+ample.addEventListener("touchend",		fTouch_onTouchEnd,		false);
+ample.addEventListener("touchcancel",	fTouch_onTouchCancel,	false);
