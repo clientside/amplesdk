36fcd0cd834a7cd582d14887a06f0c3ae9deb5a9
ilinsky <sergey@ilinsky.com>
2011-09-04 21:01:34 +0200

    DataTransfer source moved to core classes folder

--- ample/runtime/managers/DragAndDropManager.js
+++ ample/runtime/managers/DragAndDropManager.js
@@ -421,69 +421,6 @@ function fDragAndDropManager_intersectSegment(x, y, a, b)
 	return a > x ? (a <= x + y ? a + b < x + y ? b : x + y - a : 0) : (a >= x - b ? a + b < x + y ? a + b - x : y : 0);
 };
 
-//
-var cDataTransfer	= function() {
-	this.types	= new cDOMStringList;
-};
-cDataTransfer.prototype.dropEffect		= "none";			// copy|move|link|position|none (dragenter|dragover)
-cDataTransfer.prototype.effectAllowed	= "uninitialized";	// copy|move|link|copyLink|copyMove|linkMove|all|none|uninitialized=all (dragstart|dragenter|dragover)
-cDataTransfer.prototype.types			= null;
-
-cDataTransfer.prototype.clearData	= function(sFormat) {
-//->Guard
-	fGuard(arguments, [
-		["format",	cString]
-	]);
-//<-Guard
-
-	delete this.types[sFormat];
-};
-
-cDataTransfer.prototype.setData	= function(sFormat, vData) {
-//->Guard
-	fGuard(arguments, [
-		["format",	cString],
-		["data",	cString]
-	]);
-//<-Guard
-
-	this.types[sFormat]	= vData;
-};
-
-cDataTransfer.prototype.getData	= function(sFormat) {
-//->Guard
-	fGuard(arguments, [
-		["format",	cString]
-	]);
-//<-Guard
-
-	return this.types[sFormat] || null;
-};
-
-cDataTransfer.prototype.setDragImage	= function(oImage, nLeft, nTop) {
-//->Guard
-	fGuard(arguments, [
-		["image",	cXMLElement],
-		["left",	cNumber,	true],
-		["top",		cNumber,	true]
-	]);
-//<-Guard
-
-	oDragAndDropManager_image.appendChild(oImage);
-	oDragAndDropManager_image.style.marginLeft	= (nLeft || 0) + 'px';
-	oDragAndDropManager_image.style.marginTop	= (nTop || 0) + 'px';
-};
-
-cDataTransfer.prototype.addElement		= function(oElement) {
-//->Guard
-	fGuard(arguments, [
-		["element",	cXMLElement]
-	]);
-//<-Guard
-
-	oDragAndDropManager_image.appendChild(oElement);
-};
-
 // Attaching to implementation
 cElement.prototype.$draggable	= false;
 cElement.prototype.$droppable	= false;
