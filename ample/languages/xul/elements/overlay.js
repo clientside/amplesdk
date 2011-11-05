f5639cfbf3e3c88f34cb6a5ff0639919512ad59f
Tudor Holton <tudor@tudorholton.com>
2011-11-05 15:03:44 -0400

    Work in progress overlay element.

--- ample/languages/xul/elements/overlay.js
+++ ample/languages/xul/elements/overlay.js
@@ -19,6 +19,47 @@ cXULElement_overlay.handlers	= {
 	}
 };
 
+function processOverlay(oOverlayDoc) {
+    var oRootEl = oOverlayDoc.documentElement;
+    var oMatchRootEl = null;
+    if (oRootEl.hasAttribute('id')) {
+        oMatchRootEl = ample.query('//'+oRootEl.getAttribute('id'));
+    }
+    if (oMatchRootEl) processOverlayRecurse(oRootEl,aMatchRootEl);
+    else {
+        for (var iIndex = 0; iIndex < oOverlayEl.childNodes.length; iIndex++) {
+            var oChild = oOverlayEl.item(iIndex);
+            var oNewDocEl = ample.getElementById(oChild.getAttribute('id'));
+            if (oNewDocEl) processOverlayRecurse(oOverlayEl,oNewDocEl);  //Our id matches an existing element.
+        }
+    }
+
+}
+
+function processOverlayRecurse(oOverlayEl,oDocEl) {
+    //Action...
+    for (var iIndex = 0; iIndex < oOverlayEl.childNodes.length; iIndex++) {
+        var oChild = oOverlayEl.item(iIndex);
+        if (oChild.hasAttribute('id')) {
+            //We have an id
+            var oNewDocEl = ample.query(oChild.getAttribute('id'),oDocEl);
+            if (oNewDocEl) processOverlayRecurse(oOverlayEl,oNewDocEl);  //Our id matches an existing element.
+            else importAndAdd(oChild,oDocEl); //Our id doesn't insert it at the current location with the id.
+        }
+        else importAndAdd(oChild,oDocEl); //We don't have an id, so insert.
+    }
+}
+
+function importAndAdd(oNodeToAdd,oParent) {
+    var newNode = ample.importNode(oNodeToAdd);
+    if (oNodeToAdd.hasAttribute('position')) {
+        var position = oNodeToAdd.getAttribute('position');
+        oParent.insertBefore(oNodeToAdd,oParent.childNodes.item(position));
+    } else
+        oParent.appendChild(oNodeToAdd);
+}
+
+
 // Element Render: open
 cXULElement_overlay.prototype.$getTagOpen	= function() {
     return '<div class="xul-overlay' + (this.attributes["class"] ? " " + this.attributes["class"] : "") + '"><br /></div>';
