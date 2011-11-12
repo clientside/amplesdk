c73e28916f85dba32c1f56d3bf55b73b2ce657f0
Tudor Holton <tudor@tudorholton.com>
2011-11-11 22:31:37 -0500

    Basic implementation of overlay.  Only children of overlay have been tested.  Need to test additions and changes to attributes of existing element with same id.

--- ample/languages/xul/classes/cXULDocument.js
+++ ample/languages/xul/classes/cXULDocument.js
@@ -41,8 +41,58 @@ cXULDocument.prototype.loadOverlay	= function(sUrl, fObserver) {
 };
 
 function fXULDocument_applyOverlays(oDocument, oOverlay) {
-	console.log(oOverlay);
+    //If this an empty overlay node, add it to the parent and return.
+    if (!oOverlay.childNodes.length) { fXULDocument_importAndAdd(oOverlay,oDocument); return; }
+    //Action...
+    for (var iIndex = 0; iIndex < oOverlay.childNodes.length; iIndex++) {
+        var oChild = oOverlay.childNodes.item(iIndex);
+        if (oChild.hasAttribute('id')) {
+            //We have an id
+            var oNewDocEl = ample.getElementById(oChild.getAttribute('id'));
+            if (oNewDocEl) fXULDocument_applyOverlays(oNewDocEl,oChild);  //Our id matches an existing element.
+            else fXULDocument_importAndAdd(oChild,oDocument); //Our id doesn't insert it at the current location with the id.
+        }
+        else fXULDocument_importAndAdd(oChild,oDocument); //We don't have an id, so insert.
+    }
+/*
+    var oMatchRootEl = null;
+    if (oOverlay.hasAttribute('id')) {
+        oMatchRootEl = ample.query('//[id='+oOverlay.getAttribute('id')+']');
+    }
+    if (oMatchRootEl) fXULDocument_applyOverlaysRecurse(oOverlay,oMatchRootEl);
+    else {
+        for (var iIndex = 0; iIndex < oOverlay.childNodes.length; iIndex++) {
+            var oChild = oOverlay.childNodes.item(iIndex);
+            var oNewDocEl = ample.getElementById(oChild.getAttribute('id'));
+            if (oNewDocEl) fXULDocument_applyOverlaysRecurse(oOverlay,oNewDocEl);  //Our id matches an existing element.
+        }
+    }
+*/
 };
+
+function fXULDocument_importAndAdd(oNodeToAdd,oParent) {
+    var newNode = ample.importNode(oNodeToAdd,false);
+    if (oNodeToAdd.hasAttribute('position')) {
+        var position = oNodeToAdd.getAttribute('position');
+        oParent.insertBefore(oNodeToAdd,oParent.childNodes.item(position));
+    } else
+        oParent.appendChild(oNodeToAdd);
+}
+
+
+function fXULDocument_applyOverlaysRecurse(oOverlayEl,oDocEl) {
+    //Action...
+    for (var iIndex = 0; iIndex < oOverlayEl.childNodes.length; iIndex++) {
+        var oChild = oOverlayEl.childNodes.item(iIndex);
+        if (oChild.hasAttribute('id')) {
+            //We have an id
+            var oNewDocEl = ample.query(oChild.getAttribute('id'),oDocEl);
+            if (oNewDocEl) fXULDocument_applyOverlaysRecurse(oChild,oNewDocEl);  //Our id matches an existing element.
+            else fXULDocument_importAndAdd(oChild,oDocEl); //Our id doesn't insert it at the current location with the id.
+        }
+        else fXULDocument_importAndAdd(oChild,oDocEl); //We don't have an id, so insert.
+    }
+}
 
 /*
 cXULDocument.prototype.addBroadcastListenerFor	= function(oBroadcaster, oObserver, sAttr) {
