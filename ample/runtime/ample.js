205d1129f435fc80fc2b110fd1964d2bd05853f6
ilinsky <sergey@ilinsky.com>
2012-03-29 15:27:26 +0200

    Double quotes to single quotes

--- ample/runtime/ample.js
+++ ample/runtime/ample.js
@@ -359,7 +359,7 @@ fDOMConfiguration_setParameter(oConfiguration, "comments", false); 	// in DOM-Co
 // set ample parameters
 fDOMConfiguration_setParameter(oConfiguration, "ample-module-history-fix", false);	// -> ample-history
 fDOMConfiguration_setParameter(oConfiguration, "ample-version", '@project_version@');
-fDOMConfiguration_setParameter(oConfiguration, "ample-locale", "en");
+fDOMConfiguration_setParameter(oConfiguration, "ample-locale", 'en');
 fDOMConfiguration_setParameter(oConfiguration, "ample-user-locale", oUANavigator.language || oUANavigator.userLanguage || 'en-US');
 fDOMConfiguration_setParameter(oConfiguration, "ample-user-agent", '@project_userAgent@');
 fDOMConfiguration_setParameter(oConfiguration, "ample-enable-style", true);
