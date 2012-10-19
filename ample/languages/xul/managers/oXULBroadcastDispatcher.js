f23dbbeb78c97777304d03739e47570848702f51
Tudor Holton <tudor@tudorholton.com>
2012-10-19 14:31:28 +1100

    Revert changes to files I didn't mean to change in this branch.

--- ample/languages/xul/managers/oXULBroadcastDispatcher.js
+++ ample/languages/xul/managers/oXULBroadcastDispatcher.js
@@ -1,7 +1,7 @@
 /*
  * Ample SDK - JavaScript GUI Framework
  *
- * Copyright (c) 2012 Sergey Ilinsky
+ * Copyright (c) 2012 Sergey Ilinsky, Tudor Holton
  * Dual licensed under the MIT and GPL licenses.
  * See: http://www.amplesdk.com/about/licensing/
  *
