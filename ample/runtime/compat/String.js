dc63faf7ec5424a8454dbfb1ec27071be0e83e95
ilinsky <sergey@ilinsky.com>
2012-10-23 11:17:20 +0200

    Added fixes for substr and split functions misbehaving in some IE versions

--- ample/runtime/compat/String.js
+++ ample/runtime/compat/String.js
@@ -22,3 +22,33 @@ if (!cString.prototype.trimRight)
 	fExporter_export(function() {
 		return this.replace(/\s+$/, '');
 	}, "trimRight", cString.prototype);
+
+// Fixes substr function in IE not supporting negative value for the start index
+if ("substring".substr(-6) != "string")
+	fExporter_export(function(fFunction) {
+		return function(nIndex, nLength) {
+			return fFunction.call(this, nIndex + (nIndex < 0 ? this.length : 0), nLength);
+		};
+	}(cString.prototype.substr), "substr", cString.prototype);
+
+// Fixes split function in IE not properly handling regular expression parameters
+if ("substring".split(/(b)/)[2] != "string")
+	fExporter_export(function(fFunction) {
+		return function (rValue) {
+			//
+			if (typeof rValue == "string")
+				return fFunction.call(this, rValue);
+			//
+			var aValue = [],
+				sValue = '' + this,
+				aMatch;
+			while (aMatch = sValue.match(rValue)) {
+				aValue[aValue.length]	= sValue.substr(0, aMatch.index);
+				for (var nIndex = 1, nLength = aMatch.length; nIndex < nLength; nIndex++)
+					aValue[aValue.length]	= aMatch[nIndex];
+				sValue = sValue.substr(aMatch.index + aMatch[0].length);
+			}
+			aValue[aValue.length]	= sValue;
+			return aValue;
+		};
+	}(cString.prototype.split), "split", cString.prototype);
