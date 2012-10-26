19488d4307728a04aabd40d0f81bf8bb51584f71
ilinsky <sergey@ilinsky.com>
2012-10-26 10:42:23 +0200

    Broadcasting taken into oXULBroadcastDispatcher

--- ample/languages/xul/managers/oXULCommandDispatcher.js
+++ ample/languages/xul/managers/oXULCommandDispatcher.js
@@ -35,11 +35,6 @@ var oXULCommandDispatcher	= (function () {
 					if (oElement.attributes.hasOwnProperty(sName))
 						if (sName != "id" && sName != "persist")
 							oEvent.target.setAttribute(sName, oElement.attributes[sName]);
-			if ((sValue = oEvent.target.attributes["observes"]) && (oElement = this.getElementById(sValue)) && oElement instanceof cXULElement_broadcaster)
-				for (sName in oElement.attributes)
-					if (oElement.attributes.hasOwnProperty(sName))
-						if (sName != "id" && sName != "persist")
-							oEvent.target.setAttribute(sName, oElement.attributes[sName]);
 		}
 	}, true);
 
