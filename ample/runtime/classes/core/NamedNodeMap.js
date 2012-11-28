23072674a207c6860ebb26b5a9c6f68d7b297e87
ilinsky <sergey@ilinsky.com>
2012-11-28 09:55:40 +0200

    Removed private properties

--- ample/runtime/classes/core/NamedNodeMap.js
+++ ample/runtime/classes/core/NamedNodeMap.js
@@ -8,66 +8,86 @@
  */
 
 var cNamedNodeMap	= function() {
-	this.$map	= {};
-	this.$all	= [];
+
 };
 
 cNamedNodeMap.prototype.length	= 0;
 
 cNamedNodeMap.prototype.item	= function(nIndex) {
-	return this.length > nIndex ? this.$all[nIndex] : null;
+	return this.length > nIndex ? this[nIndex] : null;
+};
+
+function fNamedNodeMap_getNamedItem(oMap, sName) {
+	// Slow lookup for names which are members of NamedNodeMap object
+	if (sName in cNamedNodeMap.prototype) {
+		for (var nIndex = 0; nIndex < oMap.length; nIndex++)
+			if (oMap[nIndex].nodeName == sName)
+				return oMap[nIndex];
+		return null;
+	}
+	//
+	return oMap[sName] || null;
 };
 
 cNamedNodeMap.prototype.getNamedItem	= function(sName) {
-	return this.$map[sName] || null;
+	return fNamedNodeMap_getNamedItem(this, sName);
 };
 
 cNamedNodeMap.prototype.setNamedItem	= function(oNode) {
-	var oOldNode	= this.removeNamedItem(oNode.name);
+	var oOldNode	= fNamedNodeMap_removeItemNS(this, null, oNode.name);
 	fNamedNodeMap_addItem(this, oNode);
 	return oOldNode;
 };
 
 cNamedNodeMap.prototype.removeNamedItem	= function(sName) {
-	return fNamedNodeMap_removeItemByKey(this, sName);
+	return fNamedNodeMap_removeItemNS(this, null, sName);
 };
 
 // DOM Level 2
 cNamedNodeMap.prototype.getNamedItemNS	= function(sNameSpaceURI, sLocalName) {
-	return this.$map[(sNameSpaceURI ? sNameSpaceURI + '#' : '') + sLocalName] || null;
+	// Slow lookup for names which are members of NamedNodeMap object
+	if (sNameSpaceURI == null)
+		return fNamedNodeMap_getNamedItem(this, sLocalName);
+	//
+	for (var nIndex = 0; nIndex < this.length; nIndex++)
+		if (this[nIndex].localName == sLocalName && this[nIndex].namespaceURI == sNameSpaceURI)
+			return this[nIndex];
+	return null;
 };
 
 cNamedNodeMap.prototype.setNamedItemNS	= function(oNode) {
-	var oOldNode	= fNamedNodeMap_removeItemByKey(this, (oNode.namespaceURI ? oNode.namespaceURI + '#' : '') + oNode.localName);
+	var oOldNode	= fNamedNodeMap_removeItemNS(this, oNode.namespaceURI, oNode.localName);
 	fNamedNodeMap_addItem(this, oNode);
 	return oOldNode;
 };
 
 cNamedNodeMap.prototype.removeNamedItemNS	= function(sNameSpaceURI, sLocalName) {
-	return fNamedNodeMap_removeItemByKey(this, (sNameSpaceURI ? sNameSpaceURI + '#' : '') + sLocalName);
+	return fNamedNodeMap_removeItemNS(this, sNameSpaceURI, sLocalName);
 };
 
 function fNamedNodeMap_addItem(oMap, oNode) {
-	oMap.length++;
-	oMap.$all.push(oNode);
-	oMap.$map[oNode.nodeName]	= oNode;
+	oMap[oMap.length++]	= oNode;
+	if (!(oNode.nodeName in cNamedNodeMap.prototype))
+		oMap[oNode.nodeName]	= oNode;
 };
 
-function fNamedNodeMap_removeItemByKey(oMap, sKey) {
-	// Remove from $all collection
-	var oNode	= oMap.$map[sKey] || null;
-	if (oNode) {
-		for (var nIndex = 0, bFound	= false; nIndex < oMap.length; nIndex++)
-			if (bFound)
-				oMap.$all[nIndex - 1]	= oMap.$all[nIndex];
-			else
-			if (oMap.$all[nIndex] == oNode)
-				bFound	= true;
-		//
-		delete oMap.$all[--oMap.length];
-		// Remove from $map collection
-		delete oMap.$map[sKey];
+function fNamedNodeMap_removeItemNS(oMap, sNameSpaceURI, sLocalName) {
+	var oNode	= null;
+	for (var nIndex = 0, oItem; nIndex < oMap.length; nIndex++) {
+		oItem	= oMap[nIndex];
+		if (oNode)
+			oMap[nIndex - 1]	= oItem;
+		else
+		if (sNameSpaceURI ? oItem.namespaceURI == sNameSpaceURI && oItem.localName == sLocalName : oItem.nodeName == sLocalName)
+			oNode	= oItem;
 	}
+	// Remove if found
+	if (oNode) {
+		delete oMap[--oMap.length];
+		if (!(oNode.nodeName in cNamedNodeMap.prototype))
+			delete oMap[oNode.nodeName];
+	}
+	//
 	return oNode;
 };
 
