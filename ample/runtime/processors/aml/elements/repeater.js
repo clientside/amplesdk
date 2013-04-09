clientside/amplesdk

commit e9b069ad86ac826c7fed249ae3e5603a057dcf6c

Using variable to keep matches

ample/runtime/processors/aml/elements/repeater.js
@@ -105,20 +105,21 @@ var rAMLElement_repeater_regexp	= /(\{([^\}]+)\})/g;
 
 // 'Static' Methods
 function fAMLElement_repeater_processNode(oElement, oData, fResolver) {
-	var oNode;
+	var oNode,
+		aMatch;
 	for (var nIndex = 0; oNode	= oElement.childNodes[nIndex]; nIndex++)	{
 		switch (oNode.nodeType) {
 			case 1:	// cNode.ELEMENT_NODE
 				for (var nAttribute = 0, nLengthAttribute = oNode.attributes.length, oAttribute; nIndex < nLengthAttribute; nIndex++)
-					if ((oAttribute = oNode.attributes[nAttribute]).value.match(rAMLElement_repeater_regexp))
-						fElement_setAttributeNS(oNode, oAttribute.namespaceURI, oAttribute.name, oAttribute.value.replace(cRegExp.$1, fAMLElement_repeater_resolveValue(cRegExp.$2, oData, fResolver)));
+					if (aMatch = (oAttribute = oNode.attributes[nAttribute]).value.match(rAMLElement_repeater_regexp))
+						fElement_setAttributeNS(oNode, oAttribute.namespaceURI, oAttribute.name, oAttribute.value.replace(aMatch[1], fAMLElement_repeater_resolveValue(aMatch[2], oData, fResolver)));
 				fAMLElement_repeater_processNode(oNode, oData, fResolver);
 				break;
 
 			case 3:	// cNode.TEXT_NODE
 			case 4:	// cNode.CDATA_SECTION_NODE
-				if (oNode.data.match(rAMLElement_repeater_regexp)) {
-					oNode.data	= oNode.data.replace(cRegExp.$1, fAMLElement_repeater_resolveValue(cRegExp.$2, oData, fResolver));
+				if (aMatch = oNode.data.match(rAMLElement_repeater_regexp)) {
+					oNode.data	= oNode.data.replace(aMatch[1], fAMLElement_repeater_resolveValue(aMatch[2], oData, fResolver));
 					oNode.nodeValue	= oNode.data;
 					oNode.length= oNode.data.length;
 				}

ample/runtime/browser.js
@@ -1020,9 +1020,9 @@ function fBrowser_processScripts() {
 			// retrieve namespaces list (in older than IE6, attributes on script tag are not parsed into collection)
 			if (bTrident && (nVersion < 6 || nVersion > 8)) {
 				if (aAttributes	= oElementDOM.outerHTML.match(/<script([^\>]+)/i)[1].match(/[^=]+=("[^"]+"|[^\s]+)/gi))
-					for (var nAttribute = 0; oAttribute = aAttributes[nAttribute]; nAttribute++)
-						if (oAttribute.match(/\s([^=]+)="?([^"]+)"?/i) && (sAttribute = cRegExp.$1) != "type")
-							hAttributes[sAttribute]	= cRegExp.$2;
+					for (var nAttribute = 0, aMatch; oAttribute = aAttributes[nAttribute]; nAttribute++)
+						if ((aMatch = oAttribute.match(/\s([^=]+)="?([^"]+)"?/i)) && (sAttribute = aMatch[1]) != "type")
+							hAttributes[sAttribute]	= aMatch[2];
 			}
 			else {
 				aAttributes	= oElementDOM.attributes;

ample/runtime/modules/effects.js
@@ -13,10 +13,11 @@ function fQuery_effects_show(oQuery, fCallback, bIgnoreRuntimeCheck) {
 	fQuery_each(oQuery, function() {
 		var oElementDOM	= this.$getContainer(),
 			sValue	= this.attributes.style,
+			aMatch,
 			oStyle;
 
 		// Update model
-		if (sValue && sValue.match(rQuery_effects_display) && cRegExp.$1 == "none")
+		if (sValue && (aMatch = sValue.match(rQuery_effects_display)) && aMatch[1] == "none")
 			this.attributes.style	= sValue.replace(rQuery_effects_display, '$2');
 
 		// Update view, if available

ample/runtime/pis/xml-stylesheet.js
@@ -15,14 +15,19 @@ cXmlStylesheetPI.prototype.target	= "xml-stylesheet";
 
 cXmlStylesheetPI.handlers	= {};
 cXmlStylesheetPI.handlers["DOMNodeInserted"]	= function(oEvent) {
-	if (this.target.match(/href=["']([^"']+)["']/)) {
-		var sHref	= cRegExp.$1;
-		if (this.target.match(/type=["']([^"']+)["']/))
-			if (cRegExp.$1 == "text/css" || cRegExp.$1 == "text/ample+css") {
+	var aMatch,
+		sHref,
+		sType;
+	if (aMatch = this.target.match(/href=["']([^"']+)["']/)) {
+		sHref	= aMatch[1];
+		if (aMatch = this.target.match(/type=["']([^"']+)["']/)) {
+			sType	= aMatch[1];
+			if (sType == "text/css" || sType == "text/ample+css") {
 				var sCSS	= fBrowser_load(sHref, "text/css").responseText;
 				if (sCSS)
 					oBrowser_head.appendChild(fBrowser_createStyleSheet(sCSS, sHref));
 			}
+		}
 	}
 };
 

commit 3f90446f7f76466f6505b02c4a4f6234a4695d84

Fixed bug introduced in commit e9b069ad86ac826c7fed249ae3e5603a057dcf6c

ample/runtime/processors/aml/elements/repeater.js
@@ -101,13 +101,13 @@ cAMLElement_repeater.prototype.repeat	= function() {
 	}
 };
 
-var rAMLElement_repeater_regexp	= /(\{([^\}]+)\})/g;
+var rAMLElement_repeater_regexp	= /(\{([^\}]+)\})/;
 
 // 'Static' Methods
 function fAMLElement_repeater_processNode(oElement, oData, fResolver) {
 	var oNode,
 		aMatch;
-	for (var nIndex = 0; oNode	= oElement.childNodes[nIndex]; nIndex++)	{
+	for (var nIndex = 0; oNode	= oElement.childNodes[nIndex]; nIndex++) {
 		switch (oNode.nodeType) {
 			case 1:	// cNode.ELEMENT_NODE
 				for (var nAttribute = 0, nLengthAttribute = oNode.attributes.length, oAttribute; nIndex < nLengthAttribute; nIndex++)