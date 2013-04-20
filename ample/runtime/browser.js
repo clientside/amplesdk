f44a1098bdf3ee848ee15581f33b152c3b7c14ae
ilinsky <sergey@ilinsky.com>
2013-04-20 22:59:30 +0200

    Issue 187 - Bug in Windows 8 IE10 mouse events

--- ample/runtime/browser.js
+++ ample/runtime/browser.js
@@ -17,7 +17,10 @@ var oBrowser_factory	= oUADocument.createElement("span"),
 	hBrowser_cssColors		= fUtilities_stringToHash("aliceblue:F0F8FF;antiquewhite:FAEBD7;aqua:00FFFF;aquamarine:7FFFD4;azure:F0FFFF;beige:F5F5DC;bisque:FFE4C4;black:000000;blanchedalmond:FFEBCD;blue:0000FF;blueviolet:8A2BE2;brown:A52A2A;burlywood:DEB887;cadetblue:5F9EA0;chartreuse:7FFF00;chocolate:D2691E;coral:FF7F50;cornflowerblue:6495ED;cornsilk:FFF8DC;crimson:DC143C;cyan:00FFFF;darkblue:00008B;darkcyan:008B8B;darkgoldenrod:B8860B;darkgray:A9A9A9;darkgreen:006400;darkkhaki:BDB76B;darkmagenta:8B008B;darkolivegreen:556B2F;darkorange:FF8C00;darkorchid:9932CC;darkred:8B0000;darksalmon:E9967A;darkseagreen:8FBC8F;darkslateblue:483D8B;darkslategray:2F4F4F;darkturquoise:00CED1;darkviolet:9400D3;deeppink:FF1493;deepskyblue:00BFFF;dimgray:696969;dodgerblue:1E90FF;firebrick:B22222;floralwhite:FFFAF0;forestgreen:228B22;fuchsia:FF00FF;gainsboro:DCDCDC;ghostwhite:F8F8FF;gold:FFD700;goldenrod:DAA520;gray:808080;green:008000;greenyellow:ADFF2F;honeydew:F0FFF0;hotpink:FF69B4;indianred:CD5C5C;indigo:4B0082;ivory:FFFFF0;khaki:F0E68C;lavender:E6E6FA;lavenderblush:FFF0F5;lawngreen:7CFC00;lemonchiffon:FFFACD;lightblue:ADD8E6;lightcoral:F08080;lightcyan:E0FFFF;lightgoldenrodyellow:FAFAD2;lightgreen:90EE90;lightgrey:D3D3D3;lightpink:FFB6C1;lightsalmon:FFA07A;lightseagreen:20B2AA;lightskyblue:87CEFA;lightslategray:778899;lightsteelblue:B0C4DE;lightyellow:FFFFE0;lime:00FF00;limegreen:32CD32;linen:FAF0E6;magenta:FF00FF;maroon:800000;mediumaquamarine:66CDAA;mediumblue:0000CD;mediumorchid:BA55D3;mediumpurple:9370DB;mediumseagreen:3CB371;mediumslateblue:7B68EE;mediumspringgreen:00FA9A;mediumturquoise:48D1CC;mediumvioletred:C71585;midnightblue:191970;mintcream:F5FFFA;mistyrose:FFE4E1;moccasin:FFE4B5;navajowhite:FFDEAD;navy:000080;oldlace:FDF5E6;olive:808000;olivedrab:6B8E23;orange:FFA500;orangered:FF4500;orchid:DA70D6;palegoldenrod:EEE8AA;palegreen:98FB98;paleturquoise:AFEEEE;palevioletred:DB7093;papayawhip:FFEFD5;peachpuff:FFDAB9;peru:CD853F;pink:FFC0CB;plum:DDA0DD;powderblue:B0E0E6;purple:800080;red:FF0000;rosybrown:BC8F8F;royalblue:4169E1;saddlebrown:8B4513;salmon:FA8072;sandybrown:F4A460;seagreen:2E8B57;seashell:FFF5EE;sienna:A0522D;silver:C0C0C0;skyblue:87CEEB;slateblue:6A5ACD;slategray:708090;snow:FFFAFA;springgreen:00FF7F;steelblue:4682B4;tan:D2B48C;teal:008080;thistle:D8BFD8;tomato:FF6347;turquoise:40E0D0;violet:EE82EE;wheat:F5DEB3;white:FFFFFF;whitesmoke:F5F5F5;yellow:FFFF00;yellowgreen:9ACD32", '#'),
 	rBrowser_docType	= /<!DOCTYPE\s+(\S+)(\s+(SYSTEM|(PUBLIC\s+"([^\"]+)")))?(\s+"([^\"]+)")?(\s+\[([^\]]+)\])?\s*>/im,
 	aBrowser_mouseNodes	= new cNodeList,
-	oBrowser_mouseNode	= null,
+	oBrowser_nodeMouseOver  = null,
+	oBrowser_nodeMouseDown  = null,
+	dBrowser_nodeMouseDown  = null,
+	nBrowser_nodeMouseDown  = 1,
 	oBrowser_captureNode= null,		// Set in Capture manager
 	oBrowser_modalNode	= null,		// Set in Capture manager
 	bBrowser_mouseDown	= false,
@@ -292,7 +295,7 @@ function fBrowser_onMouseOver(oEvent) {
 		oEventMouseOver,
 		oEventMouseOut;
 
-	if (oTarget == oBrowser_mouseNode)
+	if (oTarget == oBrowser_nodeMouseOver)
 		return;
 
 	// if modal, do not dispatch event
@@ -304,13 +307,13 @@ function fBrowser_onMouseOver(oEvent) {
 	// TODO: Remove this dependency from here
 	if (!(nDragAndDropManager_dragState || nResizeManager_resizeState)) {
 		// do not dispatch event if outside modal
-		if (!oBrowser_modalNode || fBrowser_isDescendant(oBrowser_mouseNode, oBrowser_modalNode)) {
-			if (oBrowser_mouseNode && oDocument_all[oBrowser_mouseNode.uniqueID]) {
+		if (!oBrowser_modalNode || fBrowser_isDescendant(oBrowser_nodeMouseOver, oBrowser_modalNode)) {
+			if (oBrowser_nodeMouseOver && oDocument_all[oBrowser_nodeMouseOver.uniqueID]) {
 				// Create an Event
 				oEventMouseOut	= new cMouseEvent;
 				oEventMouseOut.initMouseEvent("mouseout", true, true, window, null, oEvent.screenX, oEvent.screenY, oEvent.clientX, oEvent.clientY, oEvent.ctrlKey, oEvent.altKey, oEvent.shiftKey, oEvent.metaKey, nButton, oTarget);
 				oEventMouseOut.$pseudoTarget	= oPseudo;
-				fEventTarget_dispatchEvent(oBrowser_mouseNode, oEventMouseOut);
+				fEventTarget_dispatchEvent(oBrowser_nodeMouseOver, oEventMouseOut);
 			}
 		}
 
@@ -318,14 +321,14 @@ function fBrowser_onMouseOver(oEvent) {
 		if (!oBrowser_modalNode || fBrowser_isDescendant(oTarget, oBrowser_modalNode)) {
 			// Create an Event
 			oEventMouseOver	= new cMouseEvent;
-			oEventMouseOver.initMouseEvent("mouseover", true, true, window, null, oEvent.screenX, oEvent.screenY, oEvent.clientX, oEvent.clientY, oEvent.ctrlKey, oEvent.altKey, oEvent.shiftKey, oEvent.metaKey, nButton, oBrowser_mouseNode);
+			oEventMouseOver.initMouseEvent("mouseover", true, true, window, null, oEvent.screenX, oEvent.screenY, oEvent.clientX, oEvent.clientY, oEvent.ctrlKey, oEvent.altKey, oEvent.shiftKey, oEvent.metaKey, nButton, oBrowser_nodeMouseOver);
 			oEventMouseOver.$pseudoTarget	= oPseudo;
 			fEventTarget_dispatchEvent(oTarget, oEventMouseOver);
 		}
 	}
 
 	//
-	oBrowser_mouseNode	= oTarget;
+	oBrowser_nodeMouseOver	= oTarget;
 };
 
 // Touches
@@ -582,7 +585,7 @@ function fBrowser_onClick(oEvent) {
 		bPrevent	= true;
 	}
 	// Init Click event
-	oEventClick.initMouseEvent("click", true, true, window, oEvent.detail || 1, oEvent.screenX, oEvent.screenY, oEvent.clientX, oEvent.clientY, oEvent.ctrlKey, oEvent.altKey, oEvent.shiftKey, oEvent.metaKey, nButton, null);
+	oEventClick.initMouseEvent("click", true, true, window, nBrowser_nodeMouseDown, oEvent.screenX, oEvent.screenY, oEvent.clientX, oEvent.clientY, oEvent.ctrlKey, oEvent.altKey, oEvent.shiftKey, oEvent.metaKey, nButton, null);
 	oEventClick.$pseudoTarget	= oPseudo;
 
 	// do not dispatch event if outside modal
@@ -633,11 +636,27 @@ function fBrowser_onMouseDown(oEvent) {
 	var oTarget		= fBrowser_getEventTarget(oEvent),
 		oPseudo		= fBrowser_getUIEventPseudo(oEvent),
 		nButton		= fBrowser_getUIEventButton(oEvent),
+		dMouseDate	= new cDate,
+		oMouseNode	= oEvent.target || oEvent.srcElement,
 		bCapture	= false,
 		oEventMouseDown	= new cMouseEvent;
 
 	// Flag for the dblclick fix in IE9<
 	bBrowser_mouseDown	= true;
+
+	// Fix mousedown in IE10
+	if (bTrident && nVersion == 10) {
+		if (dMouseDate - dBrowser_nodeMouseDown > 500)
+			nBrowser_nodeMouseDown	= 1;
+		else
+		if (fBrowser_isDescendant(oBrowser_nodeMouseDown, oMouseNode))
+			nBrowser_nodeMouseDown++;
+		//
+		oBrowser_nodeMouseDown	= oMouseNode;
+		dBrowser_nodeMouseDown	= dMouseDate;
+	}
+	else
+		nBrowser_nodeMouseDown	= oEvent.detail || 1;
 
 	// change target if some element is set to receive capture
 	if (oBrowser_captureNode && !fBrowser_isDescendant(oTarget, oBrowser_captureNode)) {
@@ -647,7 +666,7 @@ function fBrowser_onMouseDown(oEvent) {
 	}
 
 	// Init MouseDown event
-	oEventMouseDown.initMouseEvent("mousedown", true, true, window, oEvent.detail || 1, oEvent.screenX, oEvent.screenY, oEvent.clientX, oEvent.clientY, oEvent.ctrlKey, oEvent.altKey, oEvent.shiftKey, oEvent.metaKey, nButton, null);
+	oEventMouseDown.initMouseEvent("mousedown", true, true, window, nBrowser_nodeMouseDown, oEvent.screenX, oEvent.screenY, oEvent.clientX, oEvent.clientY, oEvent.ctrlKey, oEvent.altKey, oEvent.shiftKey, oEvent.metaKey, nButton, null);
 	oEventMouseDown.$pseudoTarget	= oPseudo;
 
 	// do not dispatch event if outside modal
@@ -698,8 +717,11 @@ function fBrowser_onMouseUp(oEvent) {
 		oPseudo	= oTarget.$getContainer();
 	}
 
+	if (!(bTrident && nVersion == 10))
+		nBrowser_nodeMouseDown	= oEvent.detail || 1;
+
 	// Init MouseUp event
-	oEventMouseUp.initMouseEvent("mouseup", true, true, window, !bBrowser_mouseDown ? 2 : oEvent.detail || 1, oEvent.screenX, oEvent.screenY, oEvent.clientX, oEvent.clientY, oEvent.ctrlKey, oEvent.altKey, oEvent.shiftKey, oEvent.metaKey, nButton, null);
+	oEventMouseUp.initMouseEvent("mouseup", true, true, window, !bBrowser_mouseDown ? 2 : nBrowser_nodeMouseDown, oEvent.screenX, oEvent.screenY, oEvent.clientX, oEvent.clientY, oEvent.ctrlKey, oEvent.altKey, oEvent.shiftKey, oEvent.metaKey, nButton, null);
 	oEventMouseUp.$pseudoTarget	= oPseudo;
 
 	// do not dispatch event if outside modal
