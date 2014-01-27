3ae85172f87a00fd07db6a4777efd20a4420876c
Sergey Ilinsky <sergey@ilinsky.com>
2014-01-27 15:57:44 +0100

    Minor normalization

--- ample/runtime/modules/queue.js
+++ ample/runtime/modules/queue.js
@@ -7,22 +7,36 @@
  *
  */
 
-oAmple.queue	= function(oElement, sType, vData) {
+// Implementation
+function fQuery_queue(oQuery, sType, vData) {
 	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+};
+
+function fQuery_dequeue(oQuery, sType) {
+	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+};
+
+function fQuery_delay(oQuery, sType) {
+	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+};
+
+// Public functions
+oAmple.queue	= function(oElement, sType, vData) {
+	return fQuery_queue.apply(new cQuery(oElement), arguments);
 };
 
 oAmple.dequeue	= function(oElement, sType) {
-	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+	return fQuery_dequeue.apply(new cQuery(oElement), arguments);
 };
 
 cQuery.prototype.queue	= function(sType, vData) {
-	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+	return fQuery_queue.apply(this, arguments);
 };
 
 cQuery.prototype.dequeue	= function(sType) {
-	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+	return fQuery_dequeue.apply(this, arguments);
 };
 
 cQuery.prototype.delay	= function(nDelay, sType) {
-	throw new cDOMException(cDOMException.NOT_SUPPORTED_ERR);
+	return fQuery_delay.apply(this, arguments);
 };
